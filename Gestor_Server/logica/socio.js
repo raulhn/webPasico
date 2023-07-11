7f7f1d43c27e33576dd1dc7f26cdb725f7a1b796
raulhn <raulhnpacheco@hotmail.com>
2023-07-11 21:05:25 +0200

    Comienzo del desarrollo de los socios

---
+++ Gestor_Server/logica/socio.js
@@ -0,0 +1,128 @@
+const conexion = require('../conexion.js')
+const constantes = require('../constantes.js')
+const persona = require('./persona.js')
+
+
+
+function existe_socio(nid_persona)
+{
+    return new Promise(
+        (resolve, reject) =>
+        {
+            conexion.dbConn.query('select count(*) cont from ' + constantes.ESQUEMA_BD + '.socios where nid_persona = ' + conexion.dbConn.escape(nid_persona),
+                (error, results, fields) =>
+                {
+                    if(error) {console.log(error); reject(error);}
+                    else { resolve(results[0]['cont']);}
+                }
+            )
+        }
+    )
+}
+
+function registrar_socio(nid_persona, fecha_alta)
+{
+    return new Promise(
+        async (resolve, reject) =>
+        {
+            bExistePersona = await persona.existe_nid(nid_persona);
+            bExisteSocio = await existe_socio(nid_persona);
+            
+            if (!bExistePersona)
+            {
+                reject('No existe la persona')
+            }
+            else if(bExisteSocio)
+            {
+                reject('El socio ya está registrado')
+            }
+            else
+            {
+                conexion.dbConn.beginTransaction(
+                    () =>
+                    {
+                        conexion.dbConn.query('insert into ' + constantes.ESQUEMA_BD + '.socios(nid_persona, fecha_alta) values(' +
+                                conexion.dbConn.escape(nid_persona) + ', ' +  'str_to_date(nullif(' + conexion.dbConn.escape(fecha_alta) + ', \'\') , \'%Y-%m-%d\')',
+                            (error, results, fields) =>
+                            {
+                                if(error)
+                                {
+                                    console.log(error);
+                                    conexion.dbConn.rollback();
+                                    reject(error);
+                                }
+                                else
+                                {
+                                    conexion.dbConn.commit();
+                                    resolve();
+                                }
+                            } 
+                        )
+                    }
+                )
+            }
+        }
+    );
+}
+
+function actualizar_socio(nid_persona, fecha_alta, fecha_baja)
+{
+    return new Promise(
+        async(resolve, reject) =>
+        {
+            bExisteSocio = await existe_socio(nid_persona);
+            if(bExisteSocio)
+            {
+                conexion.dbConn.beginTransaction(
+                    () =>
+                    {
+                        conexion.dbConn.query('update ' + constantes.ESQUEMA_BD + '.socios set fecha_baja = str_to_date(nullif(' + conexion.dbConn.escape(fecha_baja) + ', \'\') , \'%Y-%m-%d\'),' +
+                                ' fecha_alta =  str_to_date(nullif(' + conexion.dbConn.escape(fecha_alta) + ', \'\') , \'%Y-%m-%d\')' +
+                                ' where nid_persona = ' + conexion.dbConn.escape(nid_persona),
+                            (error, results, fields) =>
+                            {
+                                if(error)
+                                {
+                                    console.log(error);
+                                    conexion.dbConn.rollback();
+                                    reject(error);
+                                }
+                                else
+                                {
+                                    conexion.dbConn.commit();
+                                    resolve();
+                                }
+                            }
+                        )
+                    }
+                )
+            }
+            else
+            {
+                reject('No existe socio')
+            }
+        }
+    )
+}
+
+function obtener_socios()
+{
+    return new Promise(
+        (resolve, reject) =>
+        {
+            conexion.dbConn.query('select p.* from ' + constantes.ESQUEMA_BD + 'socios s, ' + constantes.ESQUEMA_BD + '.persona p where s.nid_persona = p.nid',
+                (error, results, fields) =>
+                {
+                    if (error) {console.log(error); reject(error)}
+                    else{
+                        resolve(results);
+                    }
+                }
+            )
+        }
+    )
+}
+
+module.exports.registrar_socio = registrar_socio;
+module.exports.actualizar_socio = actualizar_socio;
+module.exports.obtener_socios = obtener_socios;
---
+++ Gestor_Server/servlets/servlet_socios.js
@@ -0,0 +1,7 @@
+const constantes = require('../constantes.js')
+const comun = require('./servlet_comun.js')
+
+async function registrar_socio(req, res)
+{
+    
+}
