23838e721478190ae65e55f864ab9e0e1935a5e0
raulhn <raulhnpacheco@hotmail.com>
2024-05-12 12:40:02 +0200

    Se continua con el desarrollo de los horarios

--- Gestor_Server/logica/horarios.js
+++ Gestor_Server/logica/horarios.js
@@ -10,12 +10,15 @@ function registrar_horario_clase(hora_inicio, minutos_inicio, duracion_clase, nu
             conexion.dbConn.beginTransaction(
                 () =>
                 {
-                    var minutos_inicio_clase = (minutos_inicio +  duracion_clase);
-                    var hora_inicio_clase = 
+                    var minutos_totales = minutos_inicio + (hora_inicio * 60);   
+                    var minutos_comienzo_clase = minutos_totales + (duracion_clase * num_clase);
+
+                    var hora_comienzo_clase = minutos_comienzo_clase / 60;
+                    var minuto_comienzo_clase = (hora_comienzo_clase) - minutos_comienzo_clase;
 
                     conexion.dbConn.query(
                         'insert into ' + constantes.ESQUEMA_BD + '.horario_clase(hora_inicio, minutos_inicio, duracion_clase, nid_horario) ' +
-                        ' values(' + conexion.dbConn.escape(hora_inicio) + ', ' + conexion.dbConn.escape(minutos_inicio) + ', ' +
+                        ' values(' + conexion.dbConn.escape(hora_comienzo_clase) + ', ' + conexion.dbConn.escape(minuto_comienzo_clase) + ', ' +
                             conexion.dbConn.escape(duracion_clase) + ', ' + conexion.dbConn.escape(nid_horario) + ')',
                         (error, results, fields) =>
                         {
@@ -46,14 +49,20 @@ function crear_horario(dia, hora_inicio, minutos_inicio, hora_fin, minutos_fin, 
                             if(error) {console.log(error); conexion.dbConn.rollback(); reject();}
                             else {
                                 total = (hora_inicio - hora_fin) * 60;
-                                total = total + (minutos_inicio - minutos_fin) * 60
+                                total = total + (minutos_inicio - minutos_fin);
                                 num_clases = total / duracion_clase;
 
                                 for (i = 0; i < num_clases; i++)
                                 {
                                     try
                                     {
-                                        await registrar_horario_clase(hora_inicio, minutos_inicio, duracion_clase, i, results.id)
+                                        await registrar_horario_clase(hora_inicio, minutos_inicio, duracion_clase, i, results.insertId);
+                                    }
+                                    catch(error)
+                                    {
+                                        conexion.dbConn.rollback();
+                                        console.log(error);
+                                        resolve();
                                     }
                                 }
                                 conexion.dbConn.commit(); 
@@ -66,3 +75,6 @@ function crear_horario(dia, hora_inicio, minutos_inicio, hora_fin, minutos_fin, 
         }
     )
 }
+
+module.exports.registrar_horario_clase = registrar_horario_clase;
+module.exports.crear_horario = crear_horario;
