d274ce857bbc57566f74d3b6916ff6eae739c8fb
raulhn <raulhnpacheco@hotmail.com>
2024-05-20 23:43:31 +0200

    Horarios

--- Gestor/GestorPasico/src/app/componente/horario/horario.component.ts
+++ Gestor/GestorPasico/src/app/componente/horario/horario.component.ts
@@ -14,7 +14,7 @@ export class HorarioComponent implements OnInit {
   {
   }
 
-  dias = [1, 2, 3, 4, 5, 6, 7];
+  dias = [0, 1, 2, 3, 4, 5, 6, 7];
   horas:Array<number> = [];
   minutos = [0, 15, 30, 45]
 
@@ -25,7 +25,7 @@ export class HorarioComponent implements OnInit {
   hora_minima: number = 0;
   hora_maxima: number = 0;
 
-  horarios_recuperados_dia: any[] = [];
+  horarios_recuperados_dia:any = {};
 
   bCargado_horario: boolean = false;
 
@@ -33,10 +33,6 @@ export class HorarioComponent implements OnInit {
   {
     next: (respuesta: any) =>
       {
-        for(let i = this.hora_minima; i < this.hora_maxima + 1; i++)
-        {
-          this.horas.push(i);
-        }
 
         this.horarios_recuperados = respuesta.horarios_clase;
 
@@ -47,16 +43,26 @@ export class HorarioComponent implements OnInit {
             this.hora_minima = this.horarios_recuperados[i].hora_inicio;
           }
 
-          let hora_fin =  this.horarios_recuperados[i].hora_inicio + (this.horarios_recuperados[i].duracion_clase) / 60;
+          let hora_fin = Number(this.horarios_recuperados[i].hora_inicio) + (Number(this.horarios_recuperados[i].duracion_clase) / 60);
+          hora_fin = Math.trunc(hora_fin);
+
           if(this.hora_maxima == 0 || this.hora_maxima < hora_fin)
           {
             this.hora_maxima = hora_fin;
           }
-          console.log(this.horarios_recuperados[i])
-          this.horarios_recuperados_dia.push(this.horarios_recuperados[i]);
+          console.log('Dia')
+          console.log(Number(this.horarios_recuperados[i].dia))
+          this.horarios_recuperados_dia[Number(this.horarios_recuperados[i].dia)] = this.horarios_recuperados[i];
        }
+       console.log('comparacion')
+       console.log(this.hora_maxima)
+       console.log(this.hora_minima)
+       for(let i = 0; i < ((this.hora_maxima - this.hora_minima) * 4) + 1; i++)
+        {
+          this.horas.push(i);
+        }
 
-
+        console.log(this.horas)
        this.bCargado_horario = true;
       }
   }
@@ -65,39 +71,44 @@ export class HorarioComponent implements OnInit {
     this.horariosServices.obtener_horario(this.nid_horario).subscribe(this.recuperar_horario)
   }
 
-  obtener_valor(dia: number, hora: number, minutos:number)
+  obtener_valor(dia: number, hora: number)
   {
     
-    let v_minutos_entrada = Number(minutos);
-    let v_hora_entrada = Number(hora);
+    let v_minutos_entrada = (Number(hora) % 4) * 15;
+    let v_hora_entrada = Number(hora) / 4;
 
     let v_total_minutos_entrada = v_minutos_entrada + (v_hora_entrada * 60)
 
-    for(let i=0; i < this.horarios_recuperados_dia[dia].length; i++)
-    {
 
-      let v_hora_inicio = Number(this.horarios_recuperados_dia[dia][i].hora_inicio);
-      let v_minutos_inicio = Number(this.horarios_recuperados_dia[dia][i].minutos_inicio);
-
-      let v_total_minutos_inicio = v_minutos_inicio + (v_hora_inicio * 60);
-
-      let v_duracion = (Number(this.horarios_recuperados_dia[dia][i].duracion) / 60);
-
-      let v_hora_fin = v_hora_inicio + (v_duracion / 60);
-      let v_minutos_fin = (v_minutos_inicio + v_duracion) % 60;
-
-      let v_total_minutos_fin = v_minutos_fin + (v_hora_fin * 60);
-
-      if (v_total_minutos_entrada == v_total_minutos_inicio)
+      if (typeof this.horarios_recuperados_dia[dia] !== 'undefined')
       {
-        return v_duracion / 15;
+        let v_hora_inicio = Number(this.horarios_recuperados_dia[dia].hora_inicio);
+        let v_minutos_inicio = Number(this.horarios_recuperados_dia[dia].minutos_inicio);
+
+        let v_total_minutos_inicio = v_minutos_inicio + (v_hora_inicio * 60);
+
+        let v_duracion = (Number(this.horarios_recuperados_dia[dia].duracion) / 60);
+
+        let v_hora_fin = v_hora_inicio + (v_duracion / 60);
+        let v_minutos_fin = (v_minutos_inicio + v_duracion) % 60;
+
+        let v_total_minutos_fin = v_minutos_fin + (v_hora_fin * 60);
+
+        if (v_total_minutos_entrada == v_total_minutos_inicio)
+        {
+          return v_duracion / 15;
+        }
+        else if (v_total_minutos_inicio < v_total_minutos_entrada &&
+                v_total_minutos_fin >= v_total_minutos_entrada)
+        {
+          return 0; 
+        }
       }
-      else if (v_total_minutos_inicio < v_total_minutos_entrada &&
-               v_total_minutos_fin >= v_total_minutos_entrada)
+      else
       {
-        return 0; 
+        return 1;
       }
-    }
+    
     return 1;
   }
 }
