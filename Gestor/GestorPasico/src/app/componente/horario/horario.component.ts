d0704afdb1a2a3aa88fa9ce2bda1285eae081d76
raulhn <raulhnpacheco@hotmail.com>
2024-05-25 18:23:30 +0200

    Horarios

--- Gestor/GestorPasico/src/app/componente/horario/horario.component.ts
+++ Gestor/GestorPasico/src/app/componente/horario/horario.component.ts
@@ -59,10 +59,7 @@ export class HorarioComponent implements OnInit {
           console.log(Number(this.horarios_recuperados[i].dia))
           this.horarios_recuperados_dia[Number(this.horarios_recuperados[i].dia)].push(this.horarios_recuperados[i]);
        }
-       console.log('comparacion')
-       console.log(this.hora_maxima)
-       console.log(this.hora_minima)
-       for(let i = 0; i < ((this.hora_maxima - this.hora_minima + 1) * 4) ; i++)
+       for(let i = 0; i < ((this.hora_maxima - this.hora_minima + 2) * 4) ; i++)
         {
           this.horas.push(i);
         }
@@ -80,11 +77,8 @@ export class HorarioComponent implements OnInit {
   {
     
     let v_minutos_entrada = (Number(hora) % 4) * 15;
-    let v_hora_entrada = Number(hora) / 4;
+    let v_hora_entrada = Math.trunc(Number(hora) / 4);
 
-    console.log('entrada')
-    console.log(v_minutos_entrada)
-    console.log(v_hora_entrada)
     let v_total_minutos_entrada = v_minutos_entrada + (v_hora_entrada * 60)
 
     for (let i=0; i<this.horarios_recuperados_dia[dia].length; i++)
@@ -93,33 +87,45 @@ export class HorarioComponent implements OnInit {
         let v_hora_inicio = Number(this.horarios_recuperados_dia[dia][i].hora_inicio);
         let v_minutos_inicio = Number(this.horarios_recuperados_dia[dia][i].minutos_inicio);
 
-        let v_total_minutos_inicio = v_minutos_inicio + ((this.hora_minima - v_hora_inicio) * 60);
+
+        let v_total_minutos_inicio = v_minutos_inicio + Math.trunc((Math.abs((this.hora_minima - v_hora_inicio))) * 60);
 
         let v_duracion = (Number(this.horarios_recuperados_dia[dia][i].duracion_clase));
 
         let v_hora_fin = v_hora_inicio + (v_duracion / 60);
         let v_minutos_fin = (v_minutos_inicio + v_duracion) % 60;
 
-        let v_total_minutos_fin = v_minutos_fin + ((v_hora_fin - v_hora_inicio) * 60);
-     
+        let v_total_minutos_fin = v_total_minutos_inicio + v_duracion;
+
+        console.log('------')
+        console.log(hora)
+        console.log(v_hora_entrada + ':' + v_minutos_entrada)
+        console.log(Math.abs((this.hora_minima - v_hora_inicio))+ ':' + v_minutos_inicio)
+
+        console.log(v_total_minutos_entrada)
+        console.log(v_total_minutos_inicio)
+        console.log(v_total_minutos_fin)
+
         if (v_total_minutos_entrada == v_total_minutos_inicio)
         {
+          console.log('Retorno' + (v_duracion / 15))
           return v_duracion / 15;
         }
         else if (v_total_minutos_inicio < v_total_minutos_entrada &&
                 v_total_minutos_fin > v_total_minutos_entrada)
         {
-          console.log('eee')
-          console.log(v_total_minutos_entrada)
-          console.log(v_total_minutos_inicio)
-          console.log(v_total_minutos_fin)
-          console.log(v_duracion)
+          console.log('Retorno 0')
           return 0; 
         }
-
-        console.log('x');
       }
-    
+    if(dia == 0 && v_minutos_entrada == 0)
+    {
+      return 4;
+    }
+    else if(dia == 0)
+    {
+      return 0;
+    }
     return 1;
   }
 }
