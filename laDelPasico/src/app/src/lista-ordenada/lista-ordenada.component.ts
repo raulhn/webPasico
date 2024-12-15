d27b8dfd1c0cc952e305ada616ae6f800c389939
raulhn <raulhnpacheco@hotmail.com>
2024-12-15 13:43:31 +0100

    Ordenación de lista

--- laDelPasico/src/app/src/lista-ordenada/lista-ordenada.component.ts
+++ laDelPasico/src/app/src/lista-ordenada/lista-ordenada.component.ts
@@ -18,15 +18,16 @@ export class ListaOrdenadaComponent implements OnInit {
   votaciones: any;
 
   orden_votaciones: any = {};
+  votos: any = {};
 
   recupera_canciones = 
   {
     next:(respuesta: any) =>
     {
-      let mitad = respuesta.canciones.length / 2;
+      let mitad = Math.trunc(respuesta.canciones.length / 2);
       let impar = respuesta.canciones.length % 2;
 
-      this.filas = mitad + impar + 1;
+      this.filas = mitad + impar;
 
       // Se inicializa el array bidimensional
       this.canciones = [this.filas];
@@ -38,14 +39,13 @@ export class ListaOrdenadaComponent implements OnInit {
       // Se asignan las canciones
       for (let i=0; i < respuesta.canciones.length; i++)
       {
-        let fila_actual = Number(i % 2);
-        let columna_actual = Number(Math.round(i / this.filas));
-        console.log('[' + fila_actual + ']' + '[' +  columna_actual + ']')
+        let columna_actual = Number(i % 2);
+        let fila_actual = Number(Math.round(i % this.filas));
         this.canciones[fila_actual][columna_actual] = respuesta.canciones[i];
       }
 
       console.log(this.canciones);
-      console.log(this.canciones[0]);
+
       this.bCargadas_canciones = true;
     }
   }
@@ -87,33 +87,71 @@ export class ListaOrdenadaComponent implements OnInit {
   }
 
 
-  ordenar_votos()
+
+   lanzar_contador(clase: string, votos: number)
+  {
+
+        const clase_contador = document.querySelectorAll("." + clase);
+
+        // https://codepen.io/JasVidal/pen/yLrZapR //
+        clase_contador.forEach((counter) => {
+            if(counter)
+            {
+              const updateCounter = () => {
+              var target = votos;
+              var count = + Number(counter.innerHTML);
+              var increment = target / 200;
+              if (count < target) {
+                counter.innerHTML = `${Math.ceil(count + increment)}`;
+                setTimeout(updateCounter, 1);
+              } 
+              else {
+                counter.innerHTML = target.toString()
+              }
+              ;
+            };
+            updateCounter();
+          }
+          });
+}
+
+
+  async ordenar_votos()
   {
     for(let i = 0; i < this.filas; i++)
     {
       for(let j = 0; j < 2; j++)
       {
 
-        if (this.canciones[i][j])
+        if (this.canciones[i][j] !== undefined)
         {
           let id_cancion_eu = this.canciones[i][j]['nid_cancion_eu'];
 
-          let fila_nueva = Number(Number(this.orden_votaciones[id_cancion_eu]) % 2);
-          let columna_nueva = Number(Math.round(Number(this.orden_votaciones[id_cancion_eu])  / this.filas));
+          console.log('orden')
+          console.log(this.orden_votaciones[id_cancion_eu])
 
-          let nombre_clase = '.c_' + id_cancion_eu;
+          let fila_nueva = Number(Math.trunc(Number(this.orden_votaciones[id_cancion_eu]) % this.filas));
 
-          let clase = document.querySelector(nombre_clase);
+          let columna_nueva = Math.trunc(this.orden_votaciones[id_cancion_eu] / this.filas)
+
+          let nombre_clase = this.obtener_clase(id_cancion_eu)
+          let nombre_clase_contador = this.obtener_clase_contador(id_cancion_eu);
+
+          let clase = document.querySelector('.' + nombre_clase);
+          let clase_contador = document.querySelector('.' + nombre_clase_contador);
 
           let dif_columna = (columna_nueva - j) * 100;
           let dif_fila = (fila_nueva - i) * 100;
 
-          let transformacion = "transform: translate(" + dif_columna + "%, "+  dif_fila + "%); transition-duration: 1s; transition-timing-function: ease-in-out; transition-property: transform;";
+          console.log(nombre_clase_contador)
+          console.log(fila_nueva + ' - ' +columna_nueva)
+          this.lanzar_contador(nombre_clase_contador, Number(this.votos[id_cancion_eu]));
 
-          console.log(nombre_clase);
-          console.log(transformacion);
+          let transformacion = "transform: translate(" + dif_columna + "%, "+  dif_fila + "%); transition-duration: 1s; transition-timing-function: ease-in-out; transition-property: transform; transition-delay: 1s";
 
           clase?.setAttribute("style", transformacion);
+
+
         }
         
       }
@@ -125,6 +163,11 @@ export class ListaOrdenadaComponent implements OnInit {
     return 'c_' + nid_cancion;
   }
 
+  obtener_clase_contador(nid_cancion: string)
+  {
+    return 'counter_' + nid_cancion;
+  }
+
   recupera_votaciones =
   {
     next: (respuesta: any) =>
@@ -134,10 +177,11 @@ export class ListaOrdenadaComponent implements OnInit {
       for (let i = 0; i < this.votaciones.length; i++)
       {
         this.orden_votaciones[this.votaciones[i]['nid_cancion_eu']] = i;
+        this.votos[this.votaciones[i]['nid_cancion_eu']] = this.votaciones[i]['votos'];
       }
 
       this.ordenar_votos();
-      console.log(this.orden_votaciones);
+      console.log(this.votos);
     }
   }
 
