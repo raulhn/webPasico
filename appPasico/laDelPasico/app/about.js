87b91b39b815fdc9bb3ff2e042347b8f08244d4f
raulhn <raulhnpacheco@hotmail.com>
2025-03-23 11:47:00 +0100

    Primeras pruebas con enrutamiento

---
+++ appPasico/laDelPasico/app/about.js
@@ -0,0 +1,20 @@
+import { View, Text } from "react-native";
+export default function About() {
+  return (
+    <View>
+      <Text>
+        La Agrupación Musical Ntra. Sra. del Pasico depende de la Asociación
+        Amigos de la Música de Torre Pacheco, que se constituyó legalmente el
+        día 26 de febrero de 1988, recogiendo una vieja aspiración popular, con
+        raíces en anteriores Bandas de Música desaparecidas y con el fin de
+        fomentar la música en el municipio. Fue su primer Presidente Don Pedro
+        Sánchez Martínez, a quien sucedería Don Diego Azorín Cuestas, Don Ginés
+        Morales Fernández, Don Julián Pérez García y, en la actualidad, Don
+        Isidoro Jesús Martínez López. En lo que se refiere a la dirección de la
+        Banda, fue su primer Director y fundador Don Vicente Abad Herráiz, a
+        quien le seguiría Don Jesús Añó Martínez, Don Angel Quereda Barceló. Don
+        José Antonio Muñoz Osete y, actualmente, Don Ginés Martínez Vera.
+      </Text>
+    </View>
+  );
+}
--- appPasico/laDelPasico/componentes/Main.jsx
+++ appPasico/laDelPasico/componentes/Main.jsx
@@ -2,7 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Noticias } from "./Noticias";
 import { Image } from "react-native";
-import { StyleSheet, View, Text} from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import { Link } from "expo-router";
 
 const logo = require("../assets/logo.png");
 
@@ -13,8 +14,8 @@ export function Main() {
         <Image source={logo} style={styles.logo} resizeMode="center"></Image>
       </View>
       <Noticias />
-      <View className="bg-red-500">
-        <Text>Footer</Text>
+      <View className="bg-blue-500">
+        <Link href="/about">Acerca de nosotors</Link>
       </View>
       <StatusBar style="auto" />
     </SafeAreaView>
