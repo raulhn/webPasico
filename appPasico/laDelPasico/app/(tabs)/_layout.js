6281b65a0612c87fb411030fca93c83600b7de91
raulhn <raulhnpacheco@hotmail.com>
2025-03-31 20:50:37 +0200

    Barra de navegación

---
+++ appPasico/laDelPasico/app/(tabs)/_layout.js
@@ -0,0 +1,52 @@
+import { Tabs } from "expo-router";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Inicio",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="home" size={30} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="escuela"
+        options={{
+          headerShown: false,
+          title: "Escuela",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="school" size={30} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="banda"
+        options={{
+          headerShown: false,
+          title: "Banda",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="trumpet" size={30} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        ScreenOptions={{
+          headerShown: false,
+        }}
+        name="asociacion"
+        options={{
+          headerShown: false,
+          title: "Asociación",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="group" size={30} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ appPasico/laDelPasico/app/(tabs)/banda.js
@@ -0,0 +1,39 @@
+import React from "react";
+import AppBar from "../../componentes/appBar.jsx"; // Adjust the path if necessary
+import { StyleSheet, View, Text } from "react-native";
+
+export default function Banda() {
+  return (
+    <View style={estilos.principal}>
+      <Text style={estilos.titulo}>Banda de Música de Torre Pacheco</Text>
+      <Text style={estilos.texto}>
+        La Banda de Música de Torre Pacheco es una agrupación musical que se
+        dedica a la interpretación de obras musicales en diferentes estilos y
+        géneros. Ofrecemos conciertos y eventos para la comunidad, promoviendo
+        la cultura musical y el arte en nuestra localidad.
+      </Text>
+    </View>
+  );
+}
+
+const estilos = StyleSheet.create({
+  principal: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  titulo: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginVertical: 10,
+    alignContent: "center",
+    textAlign: "center",
+  },
+  texto: {
+    fontSize: 20,
+    marginVertical: 10,
+    alignContent: "center",
+    textAlign: "center",
+  },
+});
