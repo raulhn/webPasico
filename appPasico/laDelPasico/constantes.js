1bfb41766855fd902a9858a2b013fed3c3018feb
raulhn <raulhnpacheco@hotmail.com>
2025-04-02 20:49:30 +0200

    Registro conexiones

--- appPasico/laDelPasico/constantes.js
+++ appPasico/laDelPasico/constantes.js
@@ -1,2 +1,2 @@
-let URL_SERVICIO = "https://ladelpasico.es/api/";
-module.exports.URL_SERVICIO = URL_SERVICIO;
+export const URL_SERVICIO = "https://ladelpasico.es/api/";
+export const URL_SERVICIO_MOVIL = "https://ladelpasico.es/api_movil/";
