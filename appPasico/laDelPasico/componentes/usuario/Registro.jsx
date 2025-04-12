2b037ca503edc4d380cc2d2615134f16be76dc70
raulhn <raulhnpacheco@hotmail.com>
2025-04-12 11:34:35 +0200

    Se prepara el servicio para el registro de usuario

--- appPasico/laDelPasico/componentes/usuario/Registro.jsx
+++ appPasico/laDelPasico/componentes/usuario/Registro.jsx
@@ -3,10 +3,35 @@ import React from "react";
 import { View, Text, Button, TextInput, Pressable, Image } from "react-native";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState, useEffect } from "react";
+import Recaptcha from "../../componentes/Recaptcha.jsx";
 
 export default function Registro() {
   const [inputActivo, setInputActivo] = React.useState(0);
   const logo = require("../../assets/logo.png");
+
+  const [recaptchaToken, setRecaptchaToken] = useState(null);
+
+  let dataRegistro = {
+    nombre,
+    primerApellido,
+    segundoApellido,
+    correo,
+    password,
+  };
+
+  useEffect(() => {
+    if (expoPushToken && recaptchaToken) {
+      // Aquí se realiza la llamada al servicio de registro
+      // Limpia el token de reCAPTCHA después de procesarlo
+      setRecaptchaToken(null);
+    }
+  }, [expoPushToken, recaptchaToken]);
+
+  // Envía el token al servidor backend
+  const handleVerify = (tokenGoogle) => {
+    setRecaptchaToken(tokenGoogle);
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
@@ -30,6 +55,7 @@ export default function Registro() {
               placeholder="Nombre"
               onFocus={() => setInputActivo(1)}
               onBlur={() => setInputActivo(0)}
+              onChangeText={(text) => setNombre((dataRegistro.nombre = text))}
               style={
                 inputActivo === 1
                   ? [estilos.inputFocus, { width: 300 }]
@@ -41,6 +67,9 @@ export default function Registro() {
               placeholder="Primer Apellido"
               onFocus={() => setInputActivo(2)}
               onBlur={() => setInputActivo(0)}
+              onChangeText={(text) =>
+                setPrimerApellido((dataRegistro.primerApellido = text))
+              }
               style={
                 inputActivo === 2
                   ? [estilos.inputFocus, { width: 300 }]
@@ -52,6 +81,9 @@ export default function Registro() {
               onFocus={() => setInputActivo(3)}
               onBlur={() => setInputActivo(0)}
               placeholder="Segundo Apellido"
+              onChangeText={(text) =>
+                setSegundoApellido((dataRegistro.segundoApellido = text))
+              }
               style={
                 inputActivo === 3
                   ? [estilos.inputFocus, { width: 300 }]
@@ -63,6 +95,7 @@ export default function Registro() {
               onFocus={() => setInputActivo(4)}
               onBlur={() => setInputActivo(0)}
               placeholder="Correo Electrónico"
+              onChangeText={(text) => setCorreo((dataRegistro.correo = text))}
               style={
                 inputActivo === 4
                   ? [estilos.inputFocus, { width: 300 }]
@@ -74,6 +107,9 @@ export default function Registro() {
               onFocus={() => setInputActivo(5)}
               onBlur={() => setInputActivo(0)}
               placeholder="Contraseña"
+              onChangeText={(text) =>
+                setContrasena((dataRegistro.password = text))
+              }
               style={
                 inputActivo === 5
                   ? [estilos.inputFocus, { width: 200 }]
@@ -101,6 +137,7 @@ export default function Registro() {
           </View>
         </View>
       </View>
+      <Recaptcha siteKey={constantesGoogle.key} onVerify={handleVerify} />
     </SafeAreaView>
   );
 }
