4641244d33cab4d8a5779180baee2a3e7890c94d
raulhn <raulhnpacheco@hotmail.com>
2025-04-15 20:46:12 +0200

    Pequeños cambios visuales

--- appPasico/laDelPasico/componentes/ComponenteGalerias.jsx
+++ appPasico/laDelPasico/componentes/ComponenteGalerias.jsx
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from "react";
 import { CardBlog, AnimatedCardBlog } from "./CardBlog.jsx";
 import { View, Text } from "react-native";
 import { StyleSheet, ActivityIndicator } from "react-native";
+import { Pressable } from "react-native";
 
 const serviceNoticias = require("../servicios/serviceNoticias.js");
 
 export default function ComponenteGalerias() {
   const [listaGalerias, obtenerGalerias] = useState([]);
   const [cargando, setCargando] = useState(true); // Estado para controlar la carga
+  const [presionado, setPresionado] = useState(null);
 
   useEffect(() => {
     serviceNoticias.obtenerGalerias().then((v_galerias) => {
@@ -31,11 +33,26 @@ export default function ComponenteGalerias() {
   return (
     <View style={{ display: "flex" }}>
       <FlatList
+        onScrollEndDrag={() => {
+          setPresionado(null); // Cambia el estado a no presionado al hacer scroll
+        }}
         style={{ flexGrow: 1, backgroundColor: "white" }}
         data={listaGalerias}
         keyExtractor={(galeria) => galeria.nid_imagen}
         renderItem={({ item }) => (
-          <AnimatedCardBlog noticia={item}></AnimatedCardBlog>
+          <Pressable
+            onTouchStart={() => {
+              setPresionado(item.nid_imagen); // Cambia el estado a presionado
+            }}
+            onTouchEnd={() => {
+              setPresionado(null); // Cambia el estado a no presionado
+            }}
+            style={
+              presionado === item.nid_imagen ? styles.tarjetaPresionada : null
+            }
+          >
+            <AnimatedCardBlog noticia={item}></AnimatedCardBlog>
+          </Pressable>
         )}
         contentContainerStyle={styles.flatListContent} // Estilo para el contenedor de la lista
       ></FlatList>
@@ -61,5 +78,9 @@ const styles = StyleSheet.create({
   },
   flatListContent: {
     flexGrow: 1, // Permite que la lista crezca dinámicamente
+  },
+  tarjetaPresionada: {
+    transform: [{ scale: 1.05 }],
+    opacity: 0.8, // Cambia la opacidad al presionar
   },
 });
--- appPasico/laDelPasico/componentes/Noticias.jsx
+++ appPasico/laDelPasico/componentes/Noticias.jsx
@@ -81,5 +81,6 @@ const styles = StyleSheet.create({
   },
   tarjetaPresionada: {
     transform: [{ scale: 1.05 }],
+    opacity: 0.8, // Cambia la opacidad al presionar
   },
 });
