d3c886f038e3d7b2ccbc223ead0ef8f2b3831f26
raulhn <raulhnpacheco@hotmail.com>
2025-06-10 22:38:38 +0200

    cambio en la estructura

--- appPasico/laDelPasico/app.config.js
+++ appPasico/laDelPasico/app.config.js
@@ -1,41 +1,43 @@
 export default {
-  expo: {
-    name: "laDelPasico",
-    slug: "laDelPasico",
-    version: "1.0.0",
-    scheme: "laDelPasico",
-    orientation: "portrait",
-    icon: "./assets/logo.png",
-    userInterfaceStyle: "light",
-    newArchEnabled: true,
-    splash: {
-      image: "./assets/splash-icon.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff",
+	  "expo": {
+    "name": "laDelPasico",
+    "slug": "laDelPasico",
+    "version": "1.0.0",
+    "scheme": "laDelPasico",
+    "orientation": "portrait",
+    "icon": "./assets/logo.png",
+    "userInterfaceStyle": "light",
+    "newArchEnabled": true,
+    "splash": {
+      "image": "./assets/splash-icon.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
     },
-    ios: {
-      supportsTablet: true,
+    "ios": {
+      "supportsTablet": true
+    }, 
+    "android": {
+      "googleServicesFile":  process.env.GOOGLE_SERVICES_JSON || "./google-services.json",
+      "package": "com.ladelpasico.app",
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/adaptive-icon.png",
+        "backgroundColor": "#ffffff"
+      }
     },
-    android: {
-      googleServicesFile:
-        process.env.GOOGLE_SERVICES_JSON || "./google-services.json",
-      package: "com.ladelpasico.app",
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#ffffff",
+    "web": {
+      "favicon": "./assets/favicon.png"
+    },
+    "plugins": [
+      "expo-router",
+      "expo-secure-store"
+    ],
+    "extra": {
+      "router": {
+        "origin": false
       },
-    },
-    web: {
-      favicon: "./assets/favicon.png",
-    },
-    plugins: [expo - router, expo - secure - store],
-    extra: {
-      router: {
-        origin: false,
-      },
-      eas: {
-        projectId: "10a7fcf1-01cb-4d13-b0a7-0bcd9e9b2aae",
-      },
-    },
-  },
+      "eas": {
+        "projectId": "10a7fcf1-01cb-4d13-b0a7-0bcd9e9b2aae"
+      }
+    }
+  }
 };
