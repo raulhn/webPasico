288ae0275746520d6ba5ffb0abe45bd78c22709c
raulhn <raulhnpacheco@hotmail.com>
2025-08-23 15:05:09 +0200

    Correcciones en el registro

--- appPasico/laDelPasico/app/(tabs)/(banda)/banda.js
+++ appPasico/laDelPasico/app/(tabs)/(banda)/banda.js
@@ -2,6 +2,10 @@ import React from "react";
 import AppBar from "../../../componentes/appBar.jsx"; // Adjust the path if necessary
 import { StyleSheet, View, Text } from "react-native";
 
+import Constants from 'expo-constants';
+
+const KEY_GOOGLE = Constants.expoConfig.extra.GOOGLE;
+
 export default function Banda() {
   return (
     <View style={estilos.principal}>
