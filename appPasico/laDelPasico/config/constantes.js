95e2713dfdd7a31cb66e756d7b49c8c3949dcb96
raulhn <raulhnpacheco@hotmail.com>
2025-08-24 12:18:54 +0200

    Nueva constante

--- appPasico/laDelPasico/config/constantes.js
+++ appPasico/laDelPasico/config/constantes.js
@@ -8,6 +8,7 @@ const ESCUELA = 2;
 const ASOCIACION = 3;
 const INDIVIDUAL = 4;
 const GENERAL = 5;
+const PUBLIC_KEY_TURNSTILE = "0x4AAAAAABLQCI15qARYrCno";
 
 module.exports.BANDA = BANDA;
 module.exports.ESCUELA = ESCUELA;
@@ -19,3 +20,5 @@ module.exports.URL_SERVICIO = URL_SERVICIO;
 module.exports.URL_SERVICIO_MOVIL = URL_SERVICIO_MOVIL;
 module.exports.COLOR_AZUL = COLOR_AZUL;
 module.exports.COLOR_ROJO = COLOR_ROJO;
+
+module.exports.PUBLIC_KEY_TURNSTILE = PUBLIC_KEY_TURNSTILE;
