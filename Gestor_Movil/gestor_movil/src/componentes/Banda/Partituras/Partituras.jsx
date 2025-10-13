285844834f546f1085ffce840cb837e50bd5c6ab
raulhn <raulhnpacheco@hotmail.com>
2025-10-13 23:27:04 +0200

    Se incluye un filtro para las partituras

--- Gestor_Movil/gestor_movil/src/componentes/Banda/Partituras/Partituras.jsx
+++ Gestor_Movil/gestor_movil/src/componentes/Banda/Partituras/Partituras.jsx
@@ -3,15 +3,24 @@ import './Partituras.css';
 import { usePartituras } from '../../../hooks/usePartituras';
 import CardPartitura from '../CardPartitura/CardPartitura.jsx';
 import { useNavigate } from 'react-router-dom';
-import { Boton, Paginacion } from '../../ComponentesUI/ComponentesUI.jsx';
+import { Boton, EntradaTexto, Paginacion } from '../../ComponentesUI/ComponentesUI.jsx';
 import Cabecera from '../../Cabecera/Cabecera.jsx';
+import { useState, useEffect } from 'react';
 
 
 export default function Partituras() {
   const { partituras, loading, error } = usePartituras();
   const navigate = useNavigate();
+  const [partiturasFiltradas, setPartiturasFiltradas] = useState([]);
 
-  const arrayPartituras = partituras.map(partitura => (
+  // Inicializar partituras filtradas cuando carguen las partituras
+  useEffect(() => {
+    if (partituras && partituras.length > 0) {
+      setPartiturasFiltradas(partituras);
+    }
+  }, [partituras]);
+
+  const arrayPartituras = partiturasFiltradas.map(partitura => (
     <CardPartitura partitura={partitura} key={partitura.nid_partitura} />
   ));
 
@@ -29,12 +38,27 @@ export default function Partituras() {
     <div className="partituras-container">
       <h1>Partituras</h1>
       <p>Aquí se mostrarán las partituras disponibles.</p>
+      <div style={{ justifyContent: "space-between", display: "flex", alignItems: "center", gap: "10px", marginTop: "20px", marginBottom: "20px" }}>
+   
       <Boton texto="Agregar Partitura" onClick={() => {navigate('/gestion/nueva_partitura')}} />
 
-        <Paginacion className="partituras-list"
-          array={arrayPartituras}
-          page_size={6}
-          page_number={1}
+      <EntradaTexto
+            valorDefecto=""
+            setTexto={(texto) => {
+              if (partituras && partituras.length > 0) {
+                let copiaPartituras = [...partituras].filter(p => 
+                  p.titulo.toLowerCase().includes(texto.toLowerCase()) ||
+                  (p.autor && p.autor.toLowerCase().includes(texto.toLowerCase()))
+                ); 
+                setPartiturasFiltradas(copiaPartituras);
+              }
+            }}
+          />
+      </div>
+      <Paginacion className="partituras-list"
+        array={arrayPartituras}
+        page_size={6}
+        page_number={1}
         />
       
       
