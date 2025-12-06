dca3377111c44e15be73d2912c3aa6e258db6d36
raulhn <raulhnpacheco@hotmail.com>
2025-12-06 23:55:22 +0100

    Se incluyen mensajes de avisos sobre el acceso privado para los que no son alumnos o miembros de la banda

---
+++ appPasico/laDelPasico/app/(tabs)/(banda)/banda_privada.jsx
@@ -0,0 +1,44 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function BandaPrivada() {
+  return (
+    <>
+      <View style={estilos.principal}>
+        <Text style={estilos.titulo}>Zona Privada de la Banda</Text>
+
+        <Text style={estilos.texto}>
+          Para acceder a contenido exclusivo de la banda tienes que estar
+          logueado y estar dado de alta como miembro de la banda
+        </Text>
+
+        <Text style={estilos.texto}>
+          Para la resolución de cualquier duda o problema comuniquese con
+          nosotros en nuestra sede
+        </Text>
+      </View>
+    </>
+  );
+}
+
+const estilos = StyleSheet.create({
+  principal: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 20,
+  },
+  titulo: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginVertical: 10,
+    alignContent: "center",
+    textAlign: "center",
+  },
+  texto: {
+    fontSize: 20,
+    marginVertical: 10,
+    alignContent: "center",
+    textAlign: "center",
+  },
+});
