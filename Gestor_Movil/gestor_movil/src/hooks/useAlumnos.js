a2aa5f473e186666257ad3f1a58c44414366dc35
raulhn <raulhnpacheco@hotmail.com>
2025-12-27 22:18:03 +0100

    Se corrige el refresco para los activos/inactivos en personas para la aplicación de gestor movil

--- Gestor_Movil/gestor_movil/src/hooks/useAlumnos.js
+++ Gestor_Movil/gestor_movil/src/hooks/useAlumnos.js
@@ -1,79 +1,98 @@
-import {useState, useEffect} from "react"
-import * as serviceMatriculaAsignatura from "../services/ServiceMatriculaAsignatura.js"
+import { useState, useEffect } from "react";
+import * as serviceMatriculaAsignatura from "../services/ServiceMatriculaAsignatura.js";
 
 export const useAlumnosAsignaturaProfesor = (nidCurso_, nidAsignatura_) => {
-    const [alumnos, setAlumnos] = useState([]);
-    const [cargando, setCargando] = useState(true);
-    const [error, setError] = useState(null);
-    const [refrescar, setRefrescar] = useState(false);
+  const [alumnos, setAlumnos] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const [error, setError] = useState(null);
+  const [refrescar, setRefrescar] = useState(false);
 
-    const [nidAsignatura, setNidAsignatura] = useState(nidAsignatura_);
-    const [nidCurso, setNidCurso] = useState(nidCurso_);
+  const [nidAsignatura, setNidAsignatura] = useState(nidAsignatura_);
+  const [nidCurso, setNidCurso] = useState(nidCurso_);
 
-    function lanzarRefresco() {
-        setRefrescar(true);
+  function lanzarRefresco() {
+    setRefrescar(true);
+  }
+
+  useEffect(() => {
+    if (nidAsignatura && nidCurso) {
+      serviceMatriculaAsignatura
+        .obtenerAlumnosAsignaturaProfesor(nidCurso, nidAsignatura)
+        .then((data) => {
+          setAlumnos(data);
+          setCargando(false);
+          setRefrescar(false);
+        })
+        .catch((error) => {
+          console.error(
+            "Error al obtener los alumnos de la asignatura del profesor:",
+            error,
+          );
+          setError(error);
+          setCargando(false);
+          setRefrescar(false);
+        });
+    } else {
+      setAlumnos([]);
+      setRefrescar(false);
     }
+  }, [refrescar, nidCurso, nidAsignatura]);
 
-    useEffect(() => {
- 
-        if(nidAsignatura && nidCurso)      {
-        serviceMatriculaAsignatura.obtenerAlumnosAsignaturaProfesor(nidCurso, nidAsignatura)
-            .then((data) => {
-                setAlumnos(data);
-                setCargando(false);
-                setRefrescar(false);
-            })
-            .catch((error) => {
-                console.error("Error al obtener los alumnos de la asignatura del profesor:", error);
-                setError(error);
-                setCargando(false);
-                setRefrescar(false);
-            });
-        }
-        else {
-            setAlumnos([]);
-            setRefrescar(false)
-        }
-    }, [refrescar, nidCurso, nidAsignatura]);
-
-    return { alumnos, cargando, error, lanzarRefresco, setNidAsignatura, setNidCurso};
-}
+  return {
+    alumnos,
+    cargando,
+    error,
+    lanzarRefresco,
+    setNidAsignatura,
+    setNidCurso,
+  };
+};
 
 export const useAlumnosAsignatura = (nidCurso_, nidAsignatura_, activo_) => {
-    const [alumnos, setAlumnos] = useState([]);
-    const [cargando, setCargando] = useState(true);
-    const [error, setError] = useState(null);
-    const [refrescar, setRefrescar] = useState(false);
+  const [alumnos, setAlumnos] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const [error, setError] = useState(null);
+  const [refrescar, setRefrescar] = useState(false);
 
-    const [nidAsignatura, setNidAsignatura] = useState(nidAsignatura_);
-    const [nidCurso, setNidCurso] = useState(nidCurso_);
-    const [activo, setActivo] = useState(activo_);
+  const [nidAsignatura, setNidAsignatura] = useState(nidAsignatura_);
+  const [nidCurso, setNidCurso] = useState(nidCurso_);
+  const [activo, setActivo] = useState(activo_);
 
-    function lanzarRefresco() {
-        setRefrescar(true);
+  function lanzarRefresco() {
+    setRefrescar(true);
+  }
+
+  useEffect(() => {
+    if (nidAsignatura && nidCurso) {
+      serviceMatriculaAsignatura
+        .obtenerAlumnosAsignatura(nidCurso, nidAsignatura, activo)
+        .then((data) => {
+          setAlumnos(data);
+          setCargando(false);
+          setRefrescar(false);
+        })
+        .catch((error) => {
+          console.error(
+            "Error al obtener los alumnos de la asignatura:",
+            error,
+          );
+          setError(error);
+          setCargando(false);
+          setRefrescar(false);
+        });
+    } else {
+      setAlumnos([]);
+      setRefrescar(false);
     }
+  }, [refrescar, nidCurso, nidAsignatura, activo]);
 
-    useEffect(() => {
-      
-          if(nidAsignatura && nidCurso)      {
-        serviceMatriculaAsignatura.obtenerAlumnosAsignatura(nidCurso, nidAsignatura, activo)
-            .then((data) => {
-                setAlumnos(data);
-                setCargando(false);
-                setRefrescar(false);
-            })
-            .catch((error) => {
-                console.error("Error al obtener los alumnos de la asignatura:", error);
-                setError(error);
-                setCargando(false);
-                setRefrescar(false);
-            });
-          }
-          else {
-            setAlumnos([]);
-            setRefrescar(false)
-          }
-    }, [refrescar, nidCurso, nidAsignatura, activo]);
-
-    return { alumnos, cargando, error, lanzarRefresco, setNidAsignatura, setNidCurso};
-}
+  return {
+    alumnos,
+    cargando,
+    error,
+    lanzarRefresco,
+    setNidAsignatura,
+    setNidCurso,
+    setActivo,
+  };
+};
