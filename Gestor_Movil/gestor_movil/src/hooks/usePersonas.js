e6400074d4f9c216bff5381287b4463a6f401e24
raulhn <raulhnpacheco@hotmail.com>
2025-12-28 20:00:51 +0100

    Se añade listados de personas y socios

--- Gestor_Movil/gestor_movil/src/hooks/usePersonas.js
+++ Gestor_Movil/gestor_movil/src/hooks/usePersonas.js
@@ -1,7 +1,10 @@
-import { obtenerInfoPersona } from "../services/servicePersonas.js";
+import {
+  obtenerInfoPersona,
+  obtenerListadoPersonas,
+} from "../services/servicePersonas.js";
 import { useState, useEffect } from "react";
 
-export const usePersonas = (nidPersona) => {
+export const usePersona = (nidPersona) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [refrescar, setRefrescar] = useState(false);
@@ -50,4 +53,53 @@ export const usePersonas = (nidPersona) => {
   }
 
   return { info, loading, error, refresh };
+};
+
+export const usePersonas = (tipo, activo) => {
+  const [personas, setPersonas] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [refrescar, setRefrescar] = useState(false);
+
+  const [tipoPersona, setTipoPersona] = useState(tipo);
+  const [activoPersona, setActivoPersona] = useState(activo);
+
+  useEffect(() => {
+    const fetchPersonas = async () => {
+      setLoading(true);
+      setError(true);
+
+      try {
+        const data = await obtenerListadoPersonas(tipoPersona, activoPersona);
+        if (!data.error) {
+          setError(false);
+          setPersonas(data);
+          setLoading(false);
+        } else {
+          setError(true);
+          setPersonas([]);
+          setLoading(false);
+        }
+      } catch (err) {
+        console.log("Error al obtener el listado de personas:", err);
+        setError(true);
+        setLoading(false);
+        setPersonas([]);
+      }
+    };
+
+    fetchPersonas();
+  }, [tipoPersona, activoPersona, refrescar]);
+
+  function refresh() {
+    setRefrescar(!refrescar);
+  }
+  return {
+    personas,
+    loading,
+    error,
+    refresh,
+    setTipoPersona,
+    setActivoPersona,
+  };
 };
