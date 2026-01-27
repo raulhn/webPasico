df0f29f8508760d158931ca6c4d2ccdeb34babcd
raulhn <raulhnpacheco@hotmail.com>
2026-01-27 22:55:21 +0100

    Se continua con el desarollo para permitir editar eventos de agenda ya creados

--- Gestor_Server/servlets/servlet_carga_datos.js
+++ Gestor_Server/servlets/servlet_carga_datos.js
@@ -2,8 +2,18 @@ const servlet_comun = require("./servlet_comun.js");
 
 function carga_fichero(req, res) {
   servlet_comun.comprobaciones(req, res, async () => {
-    let fichero = req.files.fichero;
+    let fichero = req.body.datos;
+    console.log(req.datos);
     console.log(fichero);
+    //    console.log(req)
+    console.log(req.files);
+    console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    console.log(req.files.datos);
+    console.log("---------------------A-----------------");
+    console.log(req.files.datos.data.toString("utf-8"));
+    const textoFichero = req.files.datos.data.toString("utf-8");
+    const cadenas = textoFichero.split(/\r?\n/);
+    console.log("Cadenas", cadenas);
     res.status(200).send({ error: false, message: "Fichero recibido" });
   });
 }
