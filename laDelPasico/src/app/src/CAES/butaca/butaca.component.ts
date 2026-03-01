f88495dc69c123a9bffd059a391057263919a122
raulhn <raulhnpacheco@hotmail.com>
2026-03-01 17:22:59 +0100

    Se ralizan modificaciones par hacer clicklabces los asientos

--- laDelPasico/src/app/src/CAES/butaca/butaca.component.ts
+++ laDelPasico/src/app/src/CAES/butaca/butaca.component.ts
@@ -1,15 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 import { faCouch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-butaca',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './butaca.component.html',
   styleUrl: './butaca.component.css',
 })
 export class ButacaComponent {
   @Input() fila = 0;
   @Input() butaca: number = 0;
+  @Output() clickButaca = new EventEmitter<any>();
+
+  seleccionado: boolean = false;
 
   faCouch = faCouch;
+
+  click() {
+    console.log('x');
+    console.log('Se ha hecho click: ', this.fila, this.butaca);
+    this.seleccionado = !this.seleccionado;
+    console.log('seleccionado', this.seleccionado);
+    this.clickButaca.emit({
+      asiento: { fila: this.fila, butaca: this.butaca },
+      seleccionado: this.seleccionado,
+    });
+  }
 }
