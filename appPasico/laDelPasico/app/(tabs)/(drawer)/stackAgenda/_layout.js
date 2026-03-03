b0fd0d843dfc485e208e8cca7a049a7a0ccada8b
raulhn <raulhnpacheco@hotmail.com>
2026-03-04 00:05:14 +0100

    Se modifica el titulo del detalle de evento

--- appPasico/laDelPasico/app/(tabs)/(drawer)/stackAgenda/_layout.js
+++ appPasico/laDelPasico/app/(tabs)/(drawer)/stackAgenda/_layout.js
@@ -39,7 +39,7 @@ export default function StackAgenda() {
       <Stack.Screen
         name="evento"
         options={({ route }) => ({
-          title: route.params.titulo,
+          title: "Detalle de Evento",
           headerTitleAlign: "center", // Centra el título del encabezado
           headerStyle: {
             backgroundColor: Constantes.COLOR_AZUL, // Color de fondo del encabezado
