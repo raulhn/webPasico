71c5c597d6c6339156b14f05aa1eb870209078a0
raulhn <raulhnpacheco@hotmail.com>
2026-03-15 00:07:30 +0100

    Se elimina el tratamiento de notificaciones en Inicio

--- appPasico/laDelPasico/app/(tabs)/(drawer)/index/Inicio.jsx
+++ appPasico/laDelPasico/app/(tabs)/(drawer)/index/Inicio.jsx
@@ -1,18 +1,11 @@
 import { useContext, useEffect } from "react";
 import Main from "../../../../componentes/Main";
-
 import useNotification from "../../../../hooks/useNotification";
 import { registrarConexion } from "../../../../servicios/serviceConexion";
 import { View } from "react-native";
 import { useState } from "react";
-
 import Tunstile from "../../../../componentes/Turnstile";
 import { AuthContext } from "../../../../providers/AuthContext.js";
-
-import { router } from "expo-router";
-
-import * as Notifications from "expo-notifications";
-
 import Constantes from "../../../../config/constantes.js";
 
 export default function Inicio() {
@@ -20,33 +13,6 @@ export default function Inicio() {
   const [recaptchaToken, setRecaptchaToken] = useState(null);
 
   const { guardarTokenNotificacion } = useContext(AuthContext);
-
-  useEffect(() => {
-    const subscription = Notifications.addNotificationReceivedListener(
-      (notification) => {}
-    );
-
-    // 1. Solo usar getLastNotificationResponseAsync al montar, y solo si la app NO está en foreground
-    Notifications.getLastNotificationResponseAsync().then((notificacion) => {
-      if (notificacion) {
-        if (notificacion.notification.request.content.data) {
-          router.replace(notificacion.notification.request.content.data);
-        }
-      }
-    });
-
-    // 2. Usar el listener para cuando la app está abierta o background
-    const responseNotification =
-      Notifications.addNotificationResponseReceivedListener((notificacion) => {
-        if (notificacion.notification.request.content.data) {
-          router.push(notificacion.notification.request.content.data);
-        }
-      });
-    return () => {
-      subscription.remove();
-      responseNotification.remove();
-    };
-  }, []);
 
   useEffect(() => {
     if (expoPushToken && recaptchaToken) {
