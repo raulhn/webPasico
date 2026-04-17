74dc89c5668358274d1f343c9a9fe3678a571731
raulhn <raulhnpacheco@hotmail.com>
2026-04-18 00:38:39 +0200

    Se incluye el caso en caso de que no se pase fecha a la función de formatear

--- Gestor_Server/logica/fechas.js
+++ Gestor_Server/logica/fechas.js
@@ -1,4 +1,7 @@
 export function formatearFecha(fechaISO) {
+  if (!fechaISO || fechaISO.length === 0) {
+    return null; // Devuelve un mensaje si la fecha es nula o vacía
+  }
   const fecha = new Date(fechaISO); // Convierte la fecha ISO a un objeto Date
   const dia = String(fecha.getDate()).padStart(2, "0"); // Obtiene el día (2 dígitos)
   const mes = String(fecha.getMonth() + 1).padStart(2, "0"); // Obtiene el mes (2 dígitos, +1 porque los meses empiezan en 0)
