aa35c2c065a2c0361332a0376f01a317b55667c3
raulhn <raulhnpacheco@hotmail.com>
2026-04-18 23:26:57 +0200

    Se van incluyendo operaciones para actualizar operaciones en interfaz de socios

--- Gestor_Server/logica/interfaz_socio.js
+++ Gestor_Server/logica/interfaz_socio.js
@@ -497,6 +497,48 @@ async function obtener_nid_socio(nid_interfaz_socio) {
   }
 }
 
+function resolver_conflicto(nid_interfaz_socio, operacion) {
+  const sql =
+    "update " +
+    constantes.ESQUEMA_BD +
+    ".interfaz_socio " +
+    "set operacion = " +
+    conexion.dbConn.escape(operacion) +
+    " where nid_interfaz_socio = " +
+    conexion.dbConn.escape(nid_interfaz_socio);
+
+  return new Promise((resolve, reject) => {
+    conexion.dbConn.beginTransaction(() => {
+      conexion.dbConn.query(sql, (error, results) => {
+        if (error) {
+          console.log("interfaz_socio -> resolver_conflicto: ", error);
+          conexion.dbConn.rollback();
+          reject("Se ha producido un error al resolver el conflicto de socio");
+        } else {
+          conexion.dbConn.commit();
+          resolve(results.insertId);
+        }
+      });
+    });
+  });
+}
+
+async function resolver_conflictos(interfaz_socios) {
+  try {
+    for (const interfaz_socio of interfaz_socios) {
+      await resolver_conflicto(
+        interfaz_socio.nid_interfaz_socio,
+        interfaz_socio.operacion,
+      );
+    }
+  } catch (error) {
+    console.log("interfaz_socio -> resolver_conflictos", error);
+    throw new Error(
+      "Se ha producido un error al resolver los conflictos de socio",
+    );
+  }
+}
+
 module.exports.insertar_interfaz_socio = insertar_interfaz_socio;
 module.exports.actualizar_interfaz_socio = actualizar_interfaz_socio;
 module.exports.obtener_interfaz_socio_lote = obtener_interfaz_socio_lote;
@@ -505,3 +547,4 @@ module.exports.registrar_interfaz_socio = registrar_interfaz_socio;
 module.exports.actualizar_conflicto = actualizar_conflicto;
 module.exports.obtener_interfaz_socio_nid = obtener_interfaz_socio_nid;
 module.exports.obtener_nid_socio = obtener_nid_socio;
+module.exports.resolver_conflictos = resolver_conflictos;
