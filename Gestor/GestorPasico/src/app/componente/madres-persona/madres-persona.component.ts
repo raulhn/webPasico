5381d9072c3cb46c490353383eef6f9dc0a93643
raulhn <raulhnpacheco@hotmail.com>
2026-04-23 00:24:51 +0200

    Se modifica el mostrar los componentes de padre y madre

--- Gestor/GestorPasico/src/app/componente/madres-persona/madres-persona.component.ts
+++ Gestor/GestorPasico/src/app/componente/madres-persona/madres-persona.component.ts
@@ -3,115 +3,98 @@ import { PersonasService } from 'src/app/servicios/personas.service';
 import { URL } from 'src/app/logica/constantes';
 
 @Component({
-    selector: 'app-madres-persona',
-    templateUrl: './madres-persona.component.html',
-    styleUrls: ['./madres-persona.component.css'],
-    standalone: false
+  selector: 'app-madres-persona',
+  templateUrl: './madres-persona.component.html',
+  styleUrls: ['./madres-persona.component.css'],
+  standalone: false,
 })
 export class MadresPersonaComponent {
-  @Input() nid_persona: string="";
+  @Input() nid_persona: string = '';
 
-   lista_personas: any[] = [];
-   madre: any;
-  
+  lista_personas: any[] = [];
+  madre: any;
 
-   bError: boolean = false;
-   bRegistrado: boolean = false;
+  bError: boolean = false;
+  bRegistrado: boolean = false;
+  bCargado: boolean = false;
+  bCargadoMadre: boolean = false;
 
-   mensaje_error: string = "";
-   mensaje_registro: string = "";
+  mensaje_error: string = '';
+  mensaje_registro: string = '';
 
-   enlaceFicha: string = URL.URL_FRONT_END + "/ficha_persona/";
+  enlaceFicha: string = URL.URL_FRONT_END + '/ficha_persona/';
 
-   constructor(private personaService: PersonasService)
-   {
+  constructor(private personaService: PersonasService) {}
 
-   }
-  
-   obtener_personas =
-   {
-     next: (respuesta: any) =>
-     {
-
+  obtener_personas = {
+    next: (respuesta: any) => {
       this.lista_personas = respuesta.personas;
-     }
-   }
+      this.bCargado = true;
+    },
+  };
 
-   obtener_madre = 
-   {
-     next: (respuesta: any) =>
-     {
+  obtener_madre = {
+    next: (respuesta: any) => {
       this.madre = respuesta.madre['nid'];
-     },
-     error: (respuesta: any) =>
-     {
-      
-     }
-   }
+      this.bCargadoMadre = true;
+    },
+    error: (respuesta: any) => {},
+  };
 
-
-   ngOnInit(): void {
-      this.personaService.obtener_lista_personas().subscribe(this.obtener_personas);
-      this.personaService.obtener_madre(this.nid_persona).subscribe(this.obtener_madre);
-   }
-
-
-  registrar_madre = {
-      next: (respuesta: any) =>
-      {
-          if(!respuesta.error)
-          {
-            this.bRegistrado = true;
-            this.mensaje_registro = 'Se ha guardado correctamente'
-          }
-      },
-      error: (respuesta: any) =>
-      {
-        this.bError = true;
-        this.mensaje_error = respuesta.error.message;
-      }
+  ngOnInit(): void {
+    this.personaService
+      .obtener_lista_personas()
+      .subscribe(this.obtener_personas);
+    this.personaService
+      .obtener_madre(this.nid_persona)
+      .subscribe(this.obtener_madre);
   }
 
-
-   guardar()
-   {
-      if(this.madre === undefined)
-      {
-        this.personaService.registrar_madre(this.nid_persona, '').subscribe(this.registrar_madre);
+  registrar_madre = {
+    next: (respuesta: any) => {
+      if (!respuesta.error) {
+        this.bRegistrado = true;
+        this.mensaje_registro = 'Se ha guardado correctamente';
       }
-      else
-      {
-        this.personaService.registrar_madre(this.nid_persona, this.madre).subscribe(this.registrar_madre);
-      }
+    },
+    error: (respuesta: any) => {
+      this.bError = true;
+      this.mensaje_error = respuesta.error.message;
+    },
+  };
 
-   }
+  guardar() {
+    if (this.madre === undefined) {
+      this.personaService
+        .registrar_madre(this.nid_persona, '')
+        .subscribe(this.registrar_madre);
+    } else {
+      this.personaService
+        .registrar_madre(this.nid_persona, this.madre)
+        .subscribe(this.registrar_madre);
+    }
+  }
 
-   comparePersona_madre(item: any, selected: any) {
+  comparePersona_madre(item: any, selected: any) {
     return item['nid'] == selected;
   }
 
-  construye_peticion() : any
-  {
+  construye_peticion(): any {
     var peticion: any;
-    if(this.madre === undefined)
-    {
-      peticion = {nid_persona: this.nid_persona, nid_madre: ''};
-    }
-    else
-    {
-      peticion =  {nid_persona: this.nid_persona, nid_madre: this.madre};
+    if (this.madre === undefined) {
+      peticion = { nid_persona: this.nid_persona, nid_madre: '' };
+    } else {
+      peticion = { nid_persona: this.nid_persona, nid_madre: this.madre };
     }
 
     return peticion;
   }
 
-  obtenerEnlaceFicha()
-  {
+  obtenerEnlaceFicha() {
     return this.enlaceFicha + this.madre;
   }
 
-  existeMadre()
-  {
+  existeMadre() {
     return this.madre !== undefined && this.madre !== null;
   }
 }
--- Gestor/GestorPasico/src/app/componente/padres-persona/padres-persona.component.ts
+++ Gestor/GestorPasico/src/app/componente/padres-persona/padres-persona.component.ts
@@ -3,120 +3,104 @@ import { PersonasService } from 'src/app/servicios/personas.service';
 import { URL } from 'src/app/logica/constantes';
 
 @Component({
-    selector: 'app-padres-persona',
-    templateUrl: './padres-persona.component.html',
-    styleUrls: ['./padres-persona.component.css'],
-    standalone: false
+  selector: 'app-padres-persona',
+  templateUrl: './padres-persona.component.html',
+  styleUrls: ['./padres-persona.component.css'],
+  standalone: false,
 })
-export class PadresPersonaComponent implements OnInit{
-  @Input() nid_persona: string="";
+export class PadresPersonaComponent implements OnInit {
+  @Input() nid_persona: string = '';
 
-   lista_personas: any[] = [];
+  lista_personas: any[] = [];
 
-   padre: any;
+  padre: any;
 
-   bError: boolean = false;
-   bRegistrado: boolean = false;
+  bError: boolean = false;
+  bRegistrado: boolean = false;
 
-   mensaje_error: string = "";
-   mensaje_registro: string = "";
+  mensaje_error: string = '';
+  mensaje_registro: string = '';
 
-   enlaceFicha: string = URL.URL_FRONT_END + "/ficha_persona/";
+  enlaceFicha: string = URL.URL_FRONT_END + '/ficha_persona/';
 
-   constructor(private personaService: PersonasService)
-   {
+  bCargado: boolean = false;
+  bCargadoPadre: boolean = false;
 
-   }
-  
+  constructor(private personaService: PersonasService) {}
 
-   obtener_personas =
-   {
-     next: (respuesta: any) =>
-     {
+  obtener_personas = {
+    next: (respuesta: any) => {
       this.lista_personas = respuesta.personas;
-     }
-   }
+      this.bCargado = true;
+    },
+  };
 
-   obtener_padre = 
-   {
-     next: (respuesta: any) =>
-     {
+  obtener_padre = {
+    next: (respuesta: any) => {
       this.padre = respuesta.padre['nid'];
-     },
-     error: (respuesta: any) =>
-     {
-      
-     }
-   }
+      this.bCargadoPadre = true;
+    },
+    error: (respuesta: any) => {},
+  };
 
-
-   ngOnInit(): void {
-      this.personaService.obtener_lista_personas().subscribe(this.obtener_personas);
-      this.personaService.obtener_padre(this.nid_persona).subscribe(this.obtener_padre);
-   }
-
-
-  registrar_padre = {
-      next: (respuesta: any) =>
-      {
-          if(!respuesta.error)
-          {
-            this.bRegistrado = true;
-            this.mensaje_registro = 'Se ha guardado correctamente'
-          }
-      },
-      error: (respuesta: any) =>
-      {
-        this.bError = true;
-        this.mensaje_error = respuesta.error.message;
-      }
+  ngOnInit(): void {
+    this.personaService
+      .obtener_lista_personas()
+      .subscribe(this.obtener_personas);
+    this.personaService
+      .obtener_padre(this.nid_persona)
+      .subscribe(this.obtener_padre);
   }
 
-
-   guardar()
-   {
-      if(this.padre === undefined)
-      {
-        this.personaService.registrar_padre(this.nid_persona, '').subscribe(this.registrar_padre);
+  registrar_padre = {
+    next: (respuesta: any) => {
+      if (!respuesta.error) {
+        this.bRegistrado = true;
+        this.mensaje_registro = 'Se ha guardado correctamente';
       }
-      else
-      {
-        this.personaService.registrar_padre(this.nid_persona, this.padre).subscribe(this.registrar_padre);
-      }
+    },
+    error: (respuesta: any) => {
+      this.bError = true;
+      this.mensaje_error = respuesta.error.message;
+    },
+  };
 
-   }
+  guardar() {
+    if (this.padre === undefined) {
+      this.personaService
+        .registrar_padre(this.nid_persona, '')
+        .subscribe(this.registrar_padre);
+    } else {
+      this.personaService
+        .registrar_padre(this.nid_persona, this.padre)
+        .subscribe(this.registrar_padre);
+    }
+  }
 
-   comparePersona_madre(item: any, selected: any) {
+  comparePersona_madre(item: any, selected: any) {
     return item['nid'] == selected;
   }
 
- 
   comparePersona_padre(item: any, selected: any) {
     return item['nid'] == selected;
   }
 
-  construye_peticion() : any
-  {
+  construye_peticion(): any {
     var peticion: any;
 
-    if (this.padre === undefined)
-    {
-      peticion = {nid_persona: this.nid_persona, nid_padre: ''};
-    }
-    else 
-    {
-      peticion = {nid_persona: this.nid_persona, nid_padre: this.padre};
+    if (this.padre === undefined) {
+      peticion = { nid_persona: this.nid_persona, nid_padre: '' };
+    } else {
+      peticion = { nid_persona: this.nid_persona, nid_padre: this.padre };
     }
     return peticion;
   }
 
-  obtenerEnlaceFicha()
-  {
+  obtenerEnlaceFicha() {
     return this.enlaceFicha + this.padre;
   }
 
-  existePadre()
-  {
+  existePadre() {
     return this.padre !== undefined && this.padre !== null;
   }
 }
