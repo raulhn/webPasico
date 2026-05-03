d2501d457d99f287bccebfaca0ea91a7d9109580
raulhn <raulhnpacheco@hotmail.com>
2026-05-03 20:16:31 +0200

    Se añaden los servicios de alertas

---
+++ Gestor/GestorPasico/src/app/servicios/alertas.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { URL } from '../logica/constantes';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AlertasService {
+  constructor(private http: HttpClient) {}
+
+  obtener_alumnos_sin_socios() {
+    let API_URL = URL.URL_SERVICIO + '/obtener_alumnos_sin_socios';
+    return this.http.get(API_URL, { withCredentials: true });
+  }
+
+  obtener_socios_sin_pago() {
+    let API_URL = URL.URL_SERVICIO + '/obtener_socios_sin_pago';
+    return this.http.get(API_URL, { withCredentials: true });
+  }
+
+  obtener_alumnos_sin_pago() {
+    let API_URL = URL.URL_SERVICIO + '/obtener_alumnos_sin_pago';
+    return this.http.get(API_URL, { withCredentials: true });
+  }
+
+  obtener_alumnos_sin_profesor() {
+    let API_URL = URL.URL_SERVICIO + '/obtener_alumnos_sin_profesor';
+    return this.http.get(API_URL, { withCredentials: true });
+  }
+}
