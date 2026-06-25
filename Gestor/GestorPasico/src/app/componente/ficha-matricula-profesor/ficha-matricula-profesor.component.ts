1f2cb77e49172917e22ffb5c6ba6126b48023277
raulhn <raulhnpacheco@hotmail.com>
2026-06-25 18:33:47 +0200

    Se realizan nuevos cambios de datatable

--- Gestor/GestorPasico/src/app/componente/ficha-matricula-profesor/ficha-matricula-profesor.component.ts
+++ Gestor/GestorPasico/src/app/componente/ficha-matricula-profesor/ficha-matricula-profesor.component.ts
@@ -1,4 +1,12 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  WritableSignal,
+  Signal,
+  signal,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { dom, text } from '@fortawesome/fontawesome-svg-core';
 import { DataTablesOptions } from 'src/app/logica/constantes';
@@ -8,118 +16,75 @@ import { MatriculasService } from 'src/app/servicios/matriculas.service';
 import Swal from 'sweetalert2';
 
 @Component({
-    selector: 'app-ficha-matricula-profesor',
-    templateUrl: './ficha-matricula-profesor.component.html',
-    styleUrls: ['./ficha-matricula-profesor.component.css'],
-    standalone: false
+  selector: 'app-ficha-matricula-profesor',
+  templateUrl: './ficha-matricula-profesor.component.html',
+  styleUrls: ['./ficha-matricula-profesor.component.css'],
+  standalone: false,
 })
 export class FichaMatriculaProfesorComponent implements OnInit {
+  nid_matricula: string = '';
 
-  nid_matricula: string = "";
+  $lista_asignaturas: WritableSignal<any> = signal([]);
+  $id_tabla_asignaturas: Signal<string> = signal('dataTableAsignaturas');
+  cabecera_asignaturas: any[] = [
+    { title: 'Asignatura', data: 'descripcion' },
+    { title: 'Profesor', data: 'nombre_profesor' },
+    { title: 'Fecha Alta', data: 'fecha_alta' },
+    { title: 'Fecha_baja', data: 'fecha_baja' },
+  ];
 
-  lista_asignaturas: any;
   bCargadasAsignaturas: boolean = false;
 
   matricula_asignatura_seleccionada: any;
 
-  dtOptionsAsignaturas: any = {}
+  dtOptionsAsignaturas: any = {};
 
   bCargadasEvaluciones: boolean = false;
   bCargadosTrimestres: boolean = false;
 
   lista_trimestres: any;
-  trimestre_seleccionado: string = "";
+  trimestre_seleccionado: string = '';
 
   dtOptions: any = {};
 
-  evaluaciones: any;
+  $evaluaciones: WritableSignal<any> = signal([]);
+  $id_tabla_evaluaciones: Signal<string> = signal('tabla_evaluaciones');
+  cabecera_evaluaciones: any[] = [
+    { title: 'Trimestre', data: 'trimestre' },
+    { title: 'Notas', data: 'nota' },
+    { title: 'Progreso', data: 'progreso' },
+    { title: 'Comentarios', data: 'comentario' },
+  ];
 
   @ViewChild('instancia_sustituir') instancia_sustituir!: ElementRef;
 
-  constructor(private matriculasService: MatriculasService, private rutaActiva: ActivatedRoute, private cursosService: CursosService, 
-              private evaluacionesService: EvaluacionService
-  ) { 
-
+  constructor(
+    private matriculasService: MatriculasService,
+    private rutaActiva: ActivatedRoute,
+    private cursosService: CursosService,
+    private evaluacionesService: EvaluacionService,
+  ) {
     this.nid_matricula = rutaActiva.snapshot.params['nid_matricula'];
   }
 
-  peticion_obtener_asignaturas =
-  {
-    next: (respuesta: any) =>
-    {
-      this.lista_asignaturas = respuesta.matriculas_asignaturas;
+  peticion_obtener_asignaturas = {
+    next: (respuesta: any) => {
+      this.$lista_asignaturas.set(respuesta.matriculas_asignaturas);
+    },
+  };
 
-      var dataTableAsignaturas = $('#dataTableAsignaturas').DataTable();
-      dataTableAsignaturas.destroy()
+  peticion_obtener_evaluaciones = {
+    next: (respuesta: any) => {
+      this.$evaluaciones.set(respuesta.evaluaciones);
+    },
+  };
 
-      this.dtOptionsAsignaturas = {
-        data: this.lista_asignaturas,
-        columns: [
-          {title: 'Asignatura', data: 'descripcion'},
-          {title: 'Profesor', data: 'nombre_profesor'},
-          {title: 'Fecha Alta', data: 'fecha_alta'},
-          {title: 'Fecha_baja', data: 'fecha_baja'}
-        ],
-        dom: 'Bfrtip',
-        buttons: [{extend: 'excel', text: 'Generar Excel', className: 'btn btn-dark mb-3'}],
-        language: DataTablesOptions.spanish_datatables,
-        rowCallback: (row: Node, data: any[] | Object, index: number) =>
-        {
-          $('td', row).off('click');
-          $('td', row).on('click', () => {
-            $('#tabla_asignaturas tr').removeClass('selected')
-            $(row).addClass('selected');
-          });
-          return row;
-        }
-      }
-
-      this.bCargadasAsignaturas = true;
-    }
-  }
-
-  peticion_obtener_evaluaciones =
-  {
-    next: (respuesta: any) =>
-    {
-      var dataTable = $('#tabla_evaluaciones').DataTable();
-      dataTable.destroy();
-      this.evaluaciones = respuesta.evaluaciones;
-
-      this.dtOptions = {
-        data: this.evaluaciones,
-        columns: [
-          {title: 'Trimestre', data: 'trimestre'},
-          {title: 'Notas', data: 'nota'},
-          {title: 'Progreso', data: 'progreso'},
-          {title: 'Comentarios', data: 'comentario'}
-        ],
-        dom: 'Bfrtip',
-        buttons: [{extend: 'excel', text: 'Generar Excel', className: 'btn btn-dark mb-3'}],
-        language: DataTablesOptions.spanish_datatables,
-        rowCallback: (row: Node, data: any[] | Object, index: number) =>
-        {
-          $('td', row).off('click');
-          $('td', row).on('click', () => {
-            $('#tabla_evaluaciones tr').removeClass('selected');
-            $(row).addClass('selected');
-          });
-          return row;
-        }
-      }
-
-      this.bCargadasEvaluciones = true;
-    }
-  }
-
-  recuperar_trimestres =
-  {
-    next: (respuesta: any) =>
-    {
+  recuperar_trimestres = {
+    next: (respuesta: any) => {
       this.lista_trimestres = respuesta.trimestres;
       this.bCargadosTrimestres = true;
-    }
-  }
+    },
+  };
 
   descargarFichero(fichero: string) {
     const contenidoFichero = fichero;
@@ -135,49 +100,55 @@ export class FichaMatriculaProfesorComponent implements OnInit {
     window.URL.revokeObjectURL(url);
   }
 
-  peticion_generar_boletin =
-  {
-    next: (respuesta: any) =>
-    {
+  peticion_generar_boletin = {
+    next: (respuesta: any) => {
       let fichero = respuesta.fichero;
       this.descargarFichero(fichero);
-          Swal.fire({
-            icon: 'success',
-            title: 'Boletín generado',
-            text: 'Se ha generado el boletín',
-          });
+      Swal.fire({
+        icon: 'success',
+        title: 'Boletín generado',
+        text: 'Se ha generado el boletín',
+      });
     },
-    error: (respuesta: any) =>
-    {
+    error: (respuesta: any) => {
       console.log(respuesta);
       Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: 'Se ha producido un error al generar el boletín',
-          })
-    }
-  }
-
-
+        icon: 'error',
+        title: 'Error',
+        text: 'Se ha producido un error al generar el boletín',
+      });
+    },
+  };
 
   ngOnInit(): void {
-    this.matriculasService.obtener_asignatura_matriculas_rol_profesor(this.nid_matricula).subscribe(this.peticion_obtener_asignaturas);
-    this.evaluacionesService.obtener_evaluacion_matricula_asignatura_profesor(this.nid_matricula).subscribe(this.peticion_obtener_evaluaciones);
-    this.evaluacionesService.obtener_trimestres().subscribe(this.recuperar_trimestres);
+    this.matriculasService
+      .obtener_asignatura_matriculas_rol_profesor(this.nid_matricula)
+      .subscribe(this.peticion_obtener_asignaturas);
+    this.evaluacionesService
+      .obtener_evaluacion_matricula_asignatura_profesor(this.nid_matricula)
+      .subscribe(this.peticion_obtener_evaluaciones);
+    this.evaluacionesService
+      .obtener_trimestres()
+      .subscribe(this.recuperar_trimestres);
   }
-  
-  generar_boletin()
-  {
+
+  generar_boletin() {
     Swal.fire({
       title: 'Generar boletín',
       html: this.instancia_sustituir.nativeElement,
       confirmButtonText: 'Generar',
       cancelButtonText: 'Cancelar',
-      showCancelButton: true
+      showCancelButton: true,
     }).then((result) => {
       if (result.isConfirmed) {
-        this.evaluacionesService.generar_boletin_profesor(this.nid_matricula, this.trimestre_seleccionado).subscribe(this.peticion_generar_boletin);
-      }})
+        this.evaluacionesService
+          .generar_boletin_profesor(
+            this.nid_matricula,
+            this.trimestre_seleccionado,
+          )
+          .subscribe(this.peticion_generar_boletin);
+      }
+    });
   }
 
   compareTrimestre(item: any, selected: any) {
--- Gestor/GestorPasico/src/app/componente/lista-alumnos/lista-alumnos.component.ts
+++ Gestor/GestorPasico/src/app/componente/lista-alumnos/lista-alumnos.component.ts
@@ -1,121 +1,110 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  WritableSignal,
+  Signal,
+  signal,
+} from '@angular/core';
 import { nextSortDir } from '@swimlane/ngx-datatable';
 import { AsignaturasService } from 'src/app/servicios/asignaturas.service';
 import { CursosService } from 'src/app/servicios/cursos.service';
 import { PersonasService } from 'src/app/servicios/personas.service';
 import { DataTablesOptions, URL } from 'src/app/logica/constantes';
 
-
 @Component({
-    selector: 'app-lista-alumnos',
-    templateUrl: './lista-alumnos.component.html',
-    styleUrls: ['./lista-alumnos.component.css'],
-    standalone: false
+  selector: 'app-lista-alumnos',
+  templateUrl: './lista-alumnos.component.html',
+  styleUrls: ['./lista-alumnos.component.css'],
+  standalone: false,
 })
 export class ListaAlumnosComponent implements OnInit {
-  enlaceFicha: string = URL.URL_FRONT_END + "/ficha_matricula_profesor/";
+  enlaceFicha: string = URL.URL_FRONT_END + '/ficha_matricula_profesor/';
 
   lista_cursos: any;
-  lista_personas: any;
   lista_asignaturas: any;
 
+  $lista_personas: WritableSignal<any[]> = signal([]);
+  $id_tabla_personas: Signal<string> = signal('tabla_personas');
+  cabecera_personas: any[] = [
+    { title: 'Nombre', data: 'nombre' },
+    { title: 'Primer Apellido', data: 'primer_apellido' },
+    { title: 'Segundo Apellido', data: 'segundo_apellido' },
+    { title: 'Email', data: 'correo_electronico' },
+    { title: 'Teléfono', data: 'telefono' },
+  ];
+
   bCargadoCursos: boolean = false;
-  bCargadoPersonas: boolean = false;
   bCargadasAsignaturas: boolean = false;
   bObtenidoUltimoCurso: boolean = false;
 
-  curso_seleccionado: string = "";
-  asignatura_seleccionada: string = "0";
-  alumno_activo: string = "1";
+  curso_seleccionado: string = '';
+  asignatura_seleccionada: string = '0';
+  alumno_activo: string = '1';
 
-  dtOptions_personas: any = {}
+  dtOptions_personas: any = {};
 
   persona_seleccionada: any;
 
-  constructor(private cursoService: CursosService, private personasService: PersonasService, 
-              private asignaturasService: AsignaturasService, private cursosService: CursosService) { }
+  constructor(
+    private cursoService: CursosService,
+    private personasService: PersonasService,
+    private asignaturasService: AsignaturasService,
+    private cursosService: CursosService,
+  ) {}
 
-  peticion_obtener_cursos =
-  {
-    next: (respuesta: any) =>
-    {
+  peticion_obtener_cursos = {
+    next: (respuesta: any) => {
       this.lista_cursos = respuesta.cursos;
       this.bCargadoCursos = true;
-    }
-  }
+    },
+  };
 
-  peticion_obtener_asignaturas =
-  {
-    next: (respuesta: any) =>
-    {
+  peticion_obtener_asignaturas = {
+    next: (respuesta: any) => {
       this.lista_asignaturas = respuesta.asignaturas;
       this.bCargadasAsignaturas = true;
-    }
-  }
+    },
+  };
 
   ngOnInit(): void {
-    this.cursosService.obtener_nid_ultimo_curso().subscribe(this.peticion_obtener_ultimo_curso);
+    this.cursosService
+      .obtener_nid_ultimo_curso()
+      .subscribe(this.peticion_obtener_ultimo_curso);
     this.cursoService.obtener_cursos().subscribe(this.peticion_obtener_cursos);
-    this.asignaturasService.obtener_asignaturas_rol_profesor().subscribe(this.peticion_obtener_asignaturas);
+    this.asignaturasService
+      .obtener_asignaturas_rol_profesor()
+      .subscribe(this.peticion_obtener_asignaturas);
   }
 
-  peticion_obtener_personas =
-  {
-    next: (respuesta: any) =>
-    {
-      var datatable = $('#tabla_personas').DataTable();
-      datatable.destroy();
-      this.lista_personas = respuesta.alumnos;
-
-      this.dtOptions_personas =
-      {
-        data: this.lista_personas,
-        columns: [
-          {title: 'Nombre', data: 'nombre'},
-          {title: 'Primer Apellido', data: 'primer_apellido'},
-          {title: 'Segundo Apellido', data: 'segundo_apellido'},
-          {title: 'Email', data: 'correo_electronico'},
-          {title: 'Teléfono', data: 'telefono'}
-        ],
-        language: DataTablesOptions.spanish_datatables,
-        dom: 'Bfrtip',
-        buttons: [{extend: 'excel', text: 'Generar Excel', className: 'btn btn-dark mb-3'}],
-        rowCallback: (row: Node, data: any[] | Object, index: number) => {
-          $('td', row).off('click');
-          $('td', row).on('click', () => {
-            this.persona_seleccionada = data;
-            $('#tabla_personas tr').removeClass('selected');
-            $(row).addClass('selected');
-          });
-          return row;
-        }
-
-      }
-      $('#tabla_personas').DataTable(this.dtOptions_personas);
-      this.bCargadoPersonas = true;
-    }
+  click_persona(data: any) {
+    this.persona_seleccionada = data;
   }
 
+  peticion_obtener_personas = {
+    next: (respuesta: any) => {
+      this.$lista_personas.set(respuesta.alumnos);
+    },
+  };
 
-  peticion_obtener_ultimo_curso =
-  {
-    next: (respuesta: any) =>
-    {
-      this.curso_seleccionado = respuesta['nid_ultimo_curso']
+  peticion_obtener_ultimo_curso = {
+    next: (respuesta: any) => {
+      this.curso_seleccionado = respuesta['nid_ultimo_curso'];
       this.bObtenidoUltimoCurso = true;
       this.cambia_seleccion_curso();
-    }
+    },
+  };
+
+  cambia_seleccion_curso() {
+    this.personasService
+      .obtener_alumnos_rol_profesor(
+        this.asignatura_seleccionada,
+        this.alumno_activo,
+        this.curso_seleccionado,
+      )
+      .subscribe(this.peticion_obtener_personas);
   }
 
-
-  cambia_seleccion_curso()
-  {
-    this.personasService.obtener_alumnos_rol_profesor(this.asignatura_seleccionada, this.alumno_activo, this.curso_seleccionado).subscribe(this.peticion_obtener_personas);
-  }
-
-  obtenerUrlFicha()
-  {
+  obtenerUrlFicha() {
     return this.enlaceFicha + this.persona_seleccionada.nid_matricula;
   }
-
 }
