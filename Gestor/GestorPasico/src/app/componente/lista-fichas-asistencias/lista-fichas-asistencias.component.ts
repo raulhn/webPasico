f026271dc4e4c4ab8dc96bd49595178e43b5bbe4
raulhn <raulhnpacheco@hotmail.com>
2026-06-25 20:21:42 +0200

    Se incluyen nuevos datatables

--- Gestor/GestorPasico/src/app/componente/lista-fichas-asistencias/lista-fichas-asistencias.component.ts
+++ Gestor/GestorPasico/src/app/componente/lista-fichas-asistencias/lista-fichas-asistencias.component.ts
@@ -1,119 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  WritableSignal,
+  Signal,
+  signal,
+} from '@angular/core';
 import { DataTablesOptions } from 'src/app/logica/constantes';
 import { FichaAsistenciaService } from 'src/app/servicios/fichaasistencia.service';
 import Swal from 'sweetalert2';
 
 @Component({
-    selector: 'app-lista-fichas-asistencias',
-    templateUrl: './lista-fichas-asistencias.component.html',
-    styleUrls: ['./lista-fichas-asistencias.component.css'],
-    standalone: false
+  selector: 'app-lista-fichas-asistencias',
+  templateUrl: './lista-fichas-asistencias.component.html',
+  styleUrls: ['./lista-fichas-asistencias.component.css'],
+  standalone: false,
 })
-export class ListaFichasAsistenciasComponent implements OnInit{
+export class ListaFichasAsistenciasComponent implements OnInit {
+  $lista_ficha_asistencias: WritableSignal<any[]> = signal([]);
+  $id_tabla_asistencias: Signal<string> = signal('tabla_ficha_asistencias');
+  cabecera_asistencias: any[] = [{ title: 'Ficha', data: 'etiqueta' }];
 
-  lista_ficha_asistencias: any;
   bCargadas_fichas_asistencias: boolean = false;
 
-  ficha_seleccionada: string = "";
+  ficha_seleccionada: string = '';
 
-  dtOptions_fichas_asistencias: any= {}
+  dtOptions_fichas_asistencias: any = {};
 
-  URL_FICHA_ASISTENCIA = "/ficha_asistencia/";
+  URL_FICHA_ASISTENCIA = '/ficha_asistencia/';
 
+  constructor(private fichaAsistenciaService: FichaAsistenciaService) {}
 
-  constructor(private fichaAsistenciaService: FichaAsistenciaService)
-  {}
-
-  click_ficha(ficha_marcada: any)
-  {
+  click_ficha(ficha_marcada: any) {
     this.ficha_seleccionada = ficha_marcada['nid_ficha_asistencia'];
   }
 
-
-  recuperar_ficha_asistencias =
-  {
-    next: (respuesta: any) =>
-    {
-      var datatable = $('#tabla_ficha_asistencias').DataTable();
-      datatable.destroy();
-      this.lista_ficha_asistencias = respuesta['fichas_asistencias'];
-
-      this.dtOptions_fichas_asistencias =
-      {
-        language: DataTablesOptions.spanish_datatables,
-        data: this.lista_ficha_asistencias,
-        dom: 'Bfrtip',
-        responsive: true,
-        buttons: [{extend: 'excel', text: 'Generar Excel', className: 'btn btn-dark mb-3'}],
-        columns:
-        [
-          {title: 'Ficha',
-          data: 'etiqueta'
-          }],
-          rowCallback: (row: Node, data: any[] | Object, index: number) => {
-            $('td', row).off('click');
-            $('td', row).on('click', () => {
-              this.click_ficha(data);
-              $('#tabla_ficha_asistencias tr').removeClass('selected')
-              $(row).addClass('selected');
-            });
-            return row;
-            }
-        }
-        $('#tabla_ficha_asistencias').DataTable(this.dtOptions_fichas_asistencias);
+  recuperar_ficha_asistencias = {
+    next: (respuesta: any) => {
+      this.$lista_ficha_asistencias.set(respuesta['fichas_asistencias']);
       this.bCargadas_fichas_asistencias = true;
-    }
-  }
+    },
+  };
 
   ngOnInit(): void {
-    this.fichaAsistenciaService.obtener_fichas_asistencias().subscribe(this.recuperar_ficha_asistencias);
+    this.fichaAsistenciaService
+      .obtener_fichas_asistencias()
+      .subscribe(this.recuperar_ficha_asistencias);
   }
 
-  obtener_url_ficha()
-  {
+  obtener_url_ficha() {
     return this.URL_FICHA_ASISTENCIA + this.ficha_seleccionada;
   }
 
-  peticion_cancelar_ficha_asistencia =
-  {
-    next: (respuesta: any) =>
-    {
+  peticion_cancelar_ficha_asistencia = {
+    next: (respuesta: any) => {
       Swal.fire({
         icon: 'success',
         title: 'Ficha eliminada',
-        text: 'Se ha elimiando la ficha correctamente'
-      }).then(
-        () => { 
-         window.location.reload(); }
-       )
+        text: 'Se ha elimiando la ficha correctamente',
+      }).then(() => {
+        window.location.reload();
+      });
     },
-    error: (respuesta: any) =>
-    {
+    error: (respuesta: any) => {
       Swal.fire({
-              icon: 'error',
-              title: 'Oops...',
-              text: 'Se ha producido un error',
-            })
-    }
-  }
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Se ha producido un error',
+      });
+    },
+  };
 
-  cancelar_ficha_asistencia()
-  {
-    Swal.fire(
-        {
-          title: 'Eliminar ficha',
-          text: 'Se eliminará la ficha completamente',
-          confirmButtonText: 'Eliminar',
-          showCancelButton: true
-        }
-      ).then(
-        (results: any) =>
-        {
-          if(results.isConfirmed)
-          {
-            this.fichaAsistenciaService.cancelar_ficha_asistencia(this.ficha_seleccionada).subscribe(this.peticion_cancelar_ficha_asistencia);
-          }
-        }
-      )
+  cancelar_ficha_asistencia() {
+    Swal.fire({
+      title: 'Eliminar ficha',
+      text: 'Se eliminará la ficha completamente',
+      confirmButtonText: 'Eliminar',
+      showCancelButton: true,
+    }).then((results: any) => {
+      if (results.isConfirmed) {
+        this.fichaAsistenciaService
+          .cancelar_ficha_asistencia(this.ficha_seleccionada)
+          .subscribe(this.peticion_cancelar_ficha_asistencia);
+      }
+    });
   }
 }
--- Gestor/GestorPasico/src/app/componente/lista-forma-pagos/lista-forma-pagos.component.ts
+++ Gestor/GestorPasico/src/app/componente/lista-forma-pagos/lista-forma-pagos.component.ts
@@ -1,87 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  WritableSignal,
+  Signal,
+  signal,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataTablesOptions, URL } from 'src/app/logica/constantes';
 import { PersonasService } from 'src/app/servicios/personas.service';
 
 @Component({
-    selector: 'app-lista-forma-pagos',
-    templateUrl: './lista-forma-pagos.component.html',
-    styleUrls: ['./lista-forma-pagos.component.css'],
-    standalone: false
+  selector: 'app-lista-forma-pagos',
+  templateUrl: './lista-forma-pagos.component.html',
+  styleUrls: ['./lista-forma-pagos.component.css'],
+  standalone: false,
 })
-export class ListaFormaPagosComponent implements OnInit{
+export class ListaFormaPagosComponent implements OnInit {
+  $lista_forma_pagos: WritableSignal<any[]> = signal([]);
+  $id_tabla_forma_pagos: Signal<string> = signal('tabla_forma_pago');
+  cabecera_forma_pagos: any[] = [
+    { title: 'Iban', data: 'iban' },
+    { title: 'Activo', data: 'activo' },
+    {
+      title: 'Pasarela Pago',
+      data: 'nid_metodo_pasarela_pago',
+    },
+  ];
 
-  lista_forma_pagos: any;
-  id_persona: string = "";
+  id_persona: string = '';
 
   bCargadasFormaPago: boolean = false;
-  forma_pago_seleccionada: string = "";
+  forma_pago_seleccionada: string = '';
 
-  dtOptions_forma_pagos: any= {}
-  URL_FICHA_FORMA_PAGO = "/ficha_forma_pago/";
+  dtOptions_forma_pagos: any = {};
+  URL_FICHA_FORMA_PAGO = '/ficha_forma_pago/';
 
-
-  constructor(private personaServices: PersonasService, private rutaActiva: ActivatedRoute)
-  {
+  constructor(
+    private personaServices: PersonasService,
+    private rutaActiva: ActivatedRoute,
+  ) {
     this.id_persona = rutaActiva.snapshot.params['nid_persona'];
   }
 
-  click_ficha(forma_pago: any)
-  {
+  click_ficha(forma_pago: any) {
     this.forma_pago_seleccionada = forma_pago['nid'];
   }
 
-  peticion_forma_pagos =
-  {
-    next: (respuesta: any) =>
-    {
-      this.lista_forma_pagos = respuesta.formas_pago;
-
-      var datatable = $('#tabla_forma_pago').DataTable();
-      datatable.destroy();
-
-      this.dtOptions_forma_pagos =
-      {
-        language: DataTablesOptions.spanish_datatables,
-        data: this.lista_forma_pagos,
-        dom: 'Bfrtip',
-        buttons: [{extend: 'excel', text: 'Generar Excel', className: 'btn btn-dark mb-3'}],
-        columns:
-        [
-          {title: 'Iban',
-          data: 'iban'
-          },
-          {title: 'Activo',
-            data: 'activo'
-          },
-          {
-            title: 'Pasarela Pago',
-            data: 'nid_metodo_pasarela_pago'
-          }
-        ],
-          rowCallback: (row: Node, data: any[] | Object, index: number) => {
-            $('td', row).off('click');
-            $('td', row).on('click', () => {
-              this.click_ficha(data);
-              $('#tabla_forma_pago tr').removeClass('selected')
-              $(row).addClass('selected');
-            });
-            return row;
-            }
-        }
-        $('#tabla_forma_pago').DataTable(this.dtOptions_forma_pagos);
-
+  peticion_forma_pagos = {
+    next: (respuesta: any) => {
+      this.$lista_forma_pagos.set(respuesta.formas_pago);
       this.bCargadasFormaPago = true;
-    }
-  }
+    },
+  };
 
   ngOnInit(): void {
-      this.personaServices.obtener_forma_pagos_persona(this.id_persona).subscribe(this.peticion_forma_pagos);
+    this.personaServices
+      .obtener_forma_pagos_persona(this.id_persona)
+      .subscribe(this.peticion_forma_pagos);
   }
 
-  obtener_url_ficha()
-  {
-
+  obtener_url_ficha() {
     return this.URL_FICHA_FORMA_PAGO + this.forma_pago_seleccionada;
   }
 }
