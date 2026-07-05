9d41f217c3b6b3ce4ff287c06f3cd4d55b870ddf
raulhn <raulhnpacheco@hotmail.com>
2026-07-05 23:06:30 +0200

    Se añade el impor para pool

--- Servidor_movil/conexion.js
+++ Servidor_movil/conexion.js
@@ -1,6 +1,9 @@
 var mysql = require("mysql");
 var bd_connection = require("./config/bd.json");
+var bdPool = require("./config/bdPool.json");
 
 var dbConn = mysql.createConnection(bd_connection);
+const pool = mysql.createPool(bdPool);
 
 module.exports.dbConn = dbConn;
+module.exports.pool = pool;
