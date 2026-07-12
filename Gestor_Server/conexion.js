99bb5e54f8c76ed99f21ba32d47ad01408ff256c
raulhn <raulhnpacheco@hotmail.com>
2026-07-12 11:49:17 +0200

    Se añade la configuración del pool

--- Gestor_Server/conexion.js
+++ Gestor_Server/conexion.js
@@ -1,8 +1,10 @@
 var mysql = require("mysql");
 var bd_connection = require("./config/bd.json");
+const bdPool = require("./config/bdPool.json");
 var bd_connection_web = require("./config/bd_web.json");
 var dbConn = mysql.createConnection(bd_connection);
 var dbConn_web = mysql.createConnection(bd_connection_web);
 
 module.exports.dbConn = dbConn;
 module.exports.dbConn_web = dbConn_web;
+module.exports.bdPool = bdPool;
