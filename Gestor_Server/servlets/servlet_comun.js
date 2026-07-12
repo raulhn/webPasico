a3027d08e31ed4a75d257fb4af507cde30c56466
raulhn <raulhnpacheco@hotmail.com>
2026-07-12 12:25:42 +0200

    Se añade controles de errores

--- Gestor_Server/servlets/servlet_comun.js
+++ Gestor_Server/servlets/servlet_comun.js
@@ -1,73 +1,95 @@
-const gestion_usuarios = require('../logica/usuario.js')
+const gestion_usuarios = require("../logica/usuario.js");
 
-async function comprobaciones(req, res, funcion_especifica)
-{
+async function comprobaciones(req, res, funcion_especifica) {
+  try {
     let usuario = req.session.nombre;
-    if(await gestion_usuarios.existe_login(usuario))
-    {
-        if (await gestion_usuarios.esAdministrador(usuario))
-        {
-            try{
-                await funcion_especifica();
-            }
-            catch(error)
-            {
-                console.log(error);
-                res.status(400).send({error: true, message: 'Se ha producido un error', info: error})
-            }
+    if (await gestion_usuarios.existe_login(usuario)) {
+      if (await gestion_usuarios.esAdministrador(usuario)) {
+        try {
+          await funcion_especifica();
+        } catch (error) {
+          console.log(error);
+          res.status(400).send({
+            error: true,
+            message: "Se ha producido un error",
+            info: error,
+          });
         }
-        else
-        {
-            res.status(401).send({error: true, message: 'No está autorizado'});
-        }
+      } else {
+        res.status(401).send({ error: true, message: "No está autorizado" });
+      }
+    } else {
+      res.status(401).send({ error: true, message: "No está autorizado" });
     }
-    else{
-        res.status(401).send({error: true, message: 'No está autorizado'});
-    }
+  } catch (error) {
+    console.log("Error en comprobaciones:", error);
+    res.status(500).send({
+      error: true,
+      message: "Error interno del servidor",
+      info: error,
+    });
+  }
 }
 
-
-
-async function comprobaciones_profesor(req, res, funcion_especifica)
-{
+async function comprobaciones_profesor(req, res, funcion_especifica) {
+  try {
     let usuario = req.session.nombre;
 
-    
-    if(await gestion_usuarios.existe_login(usuario) && await gestion_usuarios.esProfesor(usuario))
-    {
-        try{
-            await funcion_especifica();
-        }
-        catch(error)
-        {
-            res.status(400).send({error: true, message: 'Se ha producido un error', info: error})
-        }
+    if (
+      (await gestion_usuarios.existe_login(usuario)) &&
+      (await gestion_usuarios.esProfesor(usuario))
+    ) {
+      try {
+        await funcion_especifica();
+      } catch (error) {
+        res.status(400).send({
+          error: true,
+          message: "Se ha producido un error",
+          info: error,
+        });
+      }
+    } else {
+      res.status(401).send({ error: true, message: "No está autorizado" });
     }
-    else{
-        res.status(401).send({error: true, message: 'No está autorizado'});
-    }
+  } catch (error) {
+    console.log("Error en comprobaciones_profesor:", error);
+    res.status(500).send({
+      error: true,
+      message: "Error interno del servidor",
+      info: error,
+    });
+  }
 }
 
-async function comprobaciones_general(req, res, funcion_especifica)
-{
+async function comprobaciones_general(req, res, funcion_especifica) {
+  try {
     let usuario = req.session.nombre;
 
-    if(await gestion_usuarios.existe_login(usuario))
-    {
-        try{
-            await funcion_especifica();
-        }
-        catch(error)
-        {
-            console.log(error);
-            res.status(400).send({error: true, message: 'Se ha producido un error', info: error})
-        }
+    if (await gestion_usuarios.existe_login(usuario)) {
+      try {
+        await funcion_especifica();
+      } catch (error) {
+        console.log(error);
+        res.status(400).send({
+          error: true,
+          message: "Se ha producido un error",
+          info: error,
+        });
+      }
+    } else {
+      res.status(401).send({ error: true, message: "No está autorizado" });
     }
-    else{
-        res.status(401).send({error: true, message: 'No está autorizado'});
-    }
+  } catch (error) {
+    console.log("Error en comprobaciones_general:", error);
+    res
+      .status(500)
+      .send({
+        error: true,
+        message: "Error interno del servidor",
+        info: error,
+      });
+  }
 }
-
 
 module.exports.comprobaciones = comprobaciones;
 module.exports.comprobaciones_profesor = comprobaciones_profesor;
