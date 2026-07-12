c0ee787ebcb5cce3154ed5fdc4545351c0137c46
raulhn <raulhnpacheco@hotmail.com>
2026-07-12 13:33:27 +0200

    Se modifica url para acceder a la ficha de matricula

--- Gestor/GestorPasico/src/app/componente/matriculas-alumno/matriculas-alumno.component.ts
+++ Gestor/GestorPasico/src/app/componente/matriculas-alumno/matriculas-alumno.component.ts
@@ -1,37 +1,35 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatriculasService } from 'src/app/servicios/matriculas.service';
+import { URL } from 'src/app/logica/constantes';
 
 @Component({
-    selector: 'app-matriculas-alumno',
-    templateUrl: './matriculas-alumno.component.html',
-    styleUrls: ['./matriculas-alumno.component.css'],
-    standalone: false
+  selector: 'app-matriculas-alumno',
+  templateUrl: './matriculas-alumno.component.html',
+  styleUrls: ['./matriculas-alumno.component.css'],
+  standalone: false,
 })
 export class MatriculasAlumnoComponent implements OnInit {
-
   matriculas: any[] = [];
   bCargado: boolean = false;
 
-  constructor(private matriculasService: MatriculasService)
-  {}
+  constructor(private matriculasService: MatriculasService) {}
 
-  @Input() nid_alumno: string =""
+  @Input() nid_alumno: string = '';
 
-  recuperar_matriculas =
-  {
-    next: (respuesta: any) =>
-    {
+  recuperar_matriculas = {
+    next: (respuesta: any) => {
       this.matriculas = respuesta['matriculas'];
       this.bCargado = true;
-    }
-  }
+    },
+  };
 
   ngOnInit(): void {
-    this.matriculasService.obtener_matriculas_alumno(this.nid_alumno).subscribe(this.recuperar_matriculas);
+    this.matriculasService
+      .obtener_matriculas_alumno(this.nid_alumno)
+      .subscribe(this.recuperar_matriculas);
   }
 
-  obtener_url(nid_matricula: string)
-  {
-    return '/ficha_matricula/' + nid_matricula;
+  obtener_url(nid_matricula: string) {
+    return URL.URL_FRONT_END + '/ficha_matricula/' + nid_matricula;
   }
 }
