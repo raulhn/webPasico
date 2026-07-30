33cd3a66fc6c6158181b265b27ab5fd307c26fa0
raulhn <raulhnpacheco@hotmail.com>
2026-07-30 20:08:34 +0200

    Sigo desarrollando el nuevo componente de listado de asistencias

--- Gestor_Movil/gestor_movil/src/componentes/Escuela/AsistenciasAsignatura/AsistenciaAsignatura.jsx
+++ Gestor_Movil/gestor_movil/src/componentes/Escuela/AsistenciasAsignatura/AsistenciaAsignatura.jsx
@@ -2,35 +2,54 @@ import { useAsignaturas } from "../../../hooks/useAsignaturas";
 import { useCursos } from "../../../hooks/useCursos";
 import { Selector } from "../../ComponentesUI/ComponentesUI";
 import { useState } from "react";
+import "./AsistenciaAsignatura.css";
+import { useAsistencias } from "../../../hooks/useAsistencias";
 
 export default function AsistenciaAsignatura() {
   const { asignaturas, cargando, error, lanzarRefresco } = useAsignaturas();
   const { cursos, loading: loadingCursos, error: errorCursos } = useCursos();
+  const listaAsignaturas = Array.isArray(asignaturas) ? asignaturas : [];
+  const listaCursos = Array.isArray(cursos) ? cursos : [];
 
-  const elementos_asignaturas = asignaturas
-    .map((asignatura) => ({
+  const {
+    asistencias,
+    cargando: cargandoAsistencias,
+    error: errorAsistencias,
+  } = useAsistencias(asignaturaSeleccionada, cursoSeleccionado);
+
+  const elementos_asignaturas = [
+    { valor: "", etiqueta: "Selecciona una asignatura" },
+    ...listaAsignaturas.map((asignatura) => ({
       valor: asignatura.nid_asignatura,
-      etiqueta: asignatura.nombre,
-    }))
-    .push({ valor: "", etiqueta: "Selecciona una asignatura" });
+      etiqueta: asignatura.descripcion,
+    })),
+  ];
 
-  const elementos_cursos = cursos
-    .map((curso) => ({
+  const elementos_cursos = [
+    { valor: "", etiqueta: "Selecciona un curso" },
+    ...listaCursos.map((curso) => ({
       valor: curso.nid_curso,
-      etiqueta: curso.nombre,
-    }))
-    .push({ valor: "", etiqueta: "Selecciona un curso" });
+      etiqueta: curso.descripcion,
+    })),
+  ];
 
   const [cursoSeleccionado, setCursoSeleccionado] = useState(null);
   const [asignaturaSeleccionada, setAsignaturaSeleccionada] = useState(null);
 
   return (
     <>
-      <Selector opciones={elementos_cursos} setValor={setCursoSeleccionado} />
-      <Selector
-        opciones={elementos_asignaturas}
-        setValor={setAsignaturaSeleccionada}
-      />
+      <div className="contenedor">
+        <Selector
+          opciones={elementos_cursos}
+          setValor={setCursoSeleccionado}
+          valor={cursoSeleccionado}
+        />
+        <Selector
+          opciones={elementos_asignaturas}
+          setValor={setAsignaturaSeleccionada}
+          valor={asignaturaSeleccionada}
+        />
+      </div>
     </>
   );
 }
