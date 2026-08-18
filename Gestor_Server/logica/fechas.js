4fff5e68e4442787fa9a49ae58ef2e6adfd897b3
raulhn <raulhnpacheco@hotmail.com>
2026-08-18 15:06:24 +0200

    Se modifica el orden para la fecha con guiones

--- Gestor_Server/logica/fechas.js
+++ Gestor_Server/logica/fechas.js
@@ -17,7 +17,7 @@ function formatearFechaGuion(fechaISO) {
   const dia = String(fecha.getDate()).padStart(2, "0"); // Obtiene el día (2 dígitos)
   const mes = String(fecha.getMonth() + 1).padStart(2, "0"); // Obtiene el mes (2 dígitos, +1 porque los meses empiezan en 0)
   const anio = fecha.getFullYear(); // Obtiene el año completo
-  return `${dia}-${mes}-${anio}`; // Devuelve la fecha en formato dd/mm/yyyy
+  return `${anio}-${mes}-${dia}`; // Devuelve la fecha en formato dd/mm/yyyy
 }
 
 module.exports.formatearFecha = formatearFecha;
