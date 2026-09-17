83f268453f7126904dd60b7d6d125276dbde7a18
raulhn <raulhnpacheco@hotmail.com>
2026-09-17 19:57:50 +0200

    Cambio de version

--- appPasico/laDelPasico/app.config.js
+++ appPasico/laDelPasico/app.config.js
@@ -2,7 +2,7 @@ export default {
 	  "expo": {
     "name": "La del Pasico",
     "slug": "laDelPasico",
-    "version": "1.0.2",
+    "version": "1.0.3",
     "scheme": "laDelPasico",
     "orientation": "portrait",
     "icon": "./assets/logo.png",
