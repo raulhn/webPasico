e0ae8c4cf5cf0cc165dcab896c2a5f7d12981fa2
raulhn <raulhnpacheco@hotmail.com>
2026-09-19 16:00:37 +0200

    Se modifican los imports de utilidades para incluir la extension

--- laDelPasico/src/app/src/formulario-preinscripcion-pedania/formulario-preinscripcion-pedania.component.ts
+++ laDelPasico/src/app/src/formulario-preinscripcion-pedania/formulario-preinscripcion-pedania.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 import { ServicioPreinscripcionService } from 'src/app/servicios/servicio-preinscripcion.service';
 import Swal from 'sweetalert2';
-import { generarIdempotencyKeyImpresion } from '../logica/utilidades';
+import { generarIdempotencyKeyImpresion } from '../logica/utilidades.js';
 //import { ReCaptchaV3Service } from 'ng-recaptcha';
 import { ReCaptchaV3Service } from 'ngx-captcha';
 
